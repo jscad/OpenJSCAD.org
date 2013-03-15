13f87b6cfa83788aa7274e3ff600b3ab0432a724
Rene K. Mueller <spiritdude@gmail.com>
2013-03-15 20:29:24 +0100

    separating footer from tail, move operation space

--- openjscad.js
+++ openjscad.js
@@ -850,7 +850,7 @@ OpenJsCad.Processor.prototype = {
     this.formatDropdown.style.display = ((!this.hasOutputFile)&&(this.hasValidCurrentObject))? "inline":"none";
     this.generateOutputFileButton.style.display = ((!this.hasOutputFile)&&(this.hasValidCurrentObject))? "inline":"none";
     this.downloadOutputFileLink.style.display = this.hasOutputFile? "inline":"none";
-    this.parametersdiv.style.display = (this.paramControls.length > 0)? "inline-block":"none";
+    this.parametersdiv.style.display = (this.paramControls.length > 0)? "block":"none";      // inline-block would be nicer, but then transparent area doesn't allow to rotate models anymore (confusion to user)
     this.errordiv.style.display = this.hasError? "block":"none";
     this.statusdiv.style.display = this.hasError? "none":"block";    
   },
