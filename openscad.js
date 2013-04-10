e23de312fe54e45086afd60783e64da5cc690eed
Rene K. Mueller <spiritdude@gmail.com>
2013-04-10 20:11:41 +0200

    AMF parsing on openjscad-cli is broken nodejs-0.10.3/node-jquery-1.8.3

--- openscad.js
+++ openscad.js
@@ -65,6 +65,34 @@
 //    );
 // }
 
+function JStoMeta(src) {
+   var l = src.split(/\n/);
+   var n = 0;
+   var m = [];
+   for(var i=0; ; i++) {
+      if(l[i].match(/^\/\/\s*(\S[^:]+):\s*(\S.*)/)) {
+         var k = RegExp.$1;
+         var v = RegExp.$2;
+         m[k] = v;
+         n++;
+      } else {
+         if(i>5&&n==0)
+            break;
+         else if(n>0)
+            break;
+      }
+   }
+   return m;
+}
+
+function MetaToJS(m) {
+   var s = "";
+   for(var k in m) {
+      s += "// "+k+": "+m[k]+"\n";
+   }
+   return s;
+}
+
 // wrapper functions for OpenJsCAD & OpenJSCAD.org
 
 // color table from http://www.w3.org/TR/css3-color/
