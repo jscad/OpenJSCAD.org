d56ba8a88fd76f8faf74965f37114c49190c6631
Rene K. Mueller <spiritdude@gmail.com>
2013-04-19 00:39:34 +0200

    union() no longer extrudes, but must be all CAG or CSG

--- examples/hull.jscad
+++ examples/hull.jscad
@@ -1,12 +1,13 @@
 // title: Hull() 
 // author: Rene K. Mueller
 // license: MIT License
+// date: 2013/04/18
 
 function showHull() {
    var o = Array.prototype.slice.call(arguments);
-   return union( 
-      union(o).translate([20,0,0]),    // flat single
-      hull(o).translate([-10,0,0]),    // flat convex hulled
+   return union(     // -- we have to extrude all, in order to union (we can't mix CAG and CSG)
+      linear_extrude({height: 0.1},union(o)).translate([20,0,0]),    // flat single
+      linear_extrude({height: 0.1},hull(o)).translate([-10,0,0]),    // flat convex hulled
       linear_extrude({height: 10},     // convex hulled extruded
          hull(o)).translate([-40,0,0])
    );
--- openscad.js
+++ openscad.js
@@ -303,14 +303,19 @@ function union() {
    var o,i=0,a=arguments; 
    if(a[0].length) a = a[0]; 
    
-   if((typeof(a[i]) == "object") && (a[i] instanceof CAG)) {
-      o = a[i].extrude({offset: [0,0,0.1]});    // -- convert a 2D shape to a thin solid, note: do not a[i] = a[i].extrude()
-   } else {
-      o = a[i++];                               
+   o = a[i++];
+   if(0) {     // we leave to code for now, perhaps later we allow mixed CAG/CSG union
+      if((typeof(a[i]) == "object") && (a[i] instanceof CAG)) {
+         o = a[i].extrude({offset: [0,0,0.1]});    // -- convert a 2D shape to a thin solid, note: do not a[i] = a[i].extrude()
+      } else {
+         o = a[i++];
+      }
    }
    for(; i<a.length; i++) { 
       var obj = a[i];
-      if((typeof(a[i]) == "object") && (a[i] instanceof CAG)) {
+
+      // for now disabled, later perhaps allow mixed union of CAG/CSG
+      if(0&&(typeof(a[i]) == "object") && (a[i] instanceof CAG)) {
          obj = a[i].extrude({offset: [0,0,0.1]});    // -- convert a 2D shape to a thin solid:
       }
       o = o.union(obj); 
