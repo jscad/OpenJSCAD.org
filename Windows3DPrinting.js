49a0673cd00b83ca681b4a07dcdc1d694ce22401
Michael S. Scherotter <synergist@outlook.com>
2015-11-09 19:04:28 -0800

    updated source to start 3d print on Windows 10.

--- Windows3DPrinting.js
+++ Windows3DPrinting.js
@@ -2,16 +2,130 @@
 
 (function () {
     "use strict";
-    var processor = null;
+    var cadProcessor = null,
+        intSize = 4,
+        floatSize = 8;
+
+    function getVertices(triangles) {
+        var printing3D = Windows.Graphics.Printing3D,
+            mesh = new printing3D.Printing3DMesh();
+
+        mesh.createVertexPositions(floatSize * 3 * triangles.length);
+        var buffer = mesh.getVertexPositions();
+
+        var index = 0;
+
+        var dataWriter = new Float64Array(buffer);
+        triangles.forEach(function (triangle) {
+            triangle.vertices.forEach(function(vertex) {
+                dataWriter[index] = vertex.pos.x;
+                index++;
+                dataWriter[index] = vertex.pos.y;
+                index++;
+                dataWriter[index] = vertex.pos.z;
+                index++;
+            });
+        });
+
+        mesh.vertexCount = triangles.length * 3;
+
+        return mesh;
+    }
+
+    function getIndices(triangles, mesh) {
+        var printing3D = Windows.Graphics.Printing3D;
+        var description = {
+            format: printing3D.Printing3DBufferFormat.R32G32B32A32Float,
+            stride: 7
+        };
+
+        mesh.indexCount = triangles.length * 3;
+
+        mesh.vertexPositionsDescription = description;
+
+        mesh.createTriangleIndices(intSize * description.stride * mesh.indexCount);
+
+        var buffer = mesh.getTriangleIndices();
+
+        var dataWriter = new Float64Array(buffer);
+
+        triangles.forEach(function(triangle, index) {
+            dataWriter[index * 3] = triangle.x;
+            dataWriter[1 + index * 3] = triangle.y;
+            dataWriter[2 + index * 3] = triangle.z;
+            dataWriter[3 + index * 3] = 0;
+            dataWriter[4 + index * 3] = 0;
+            dataWriter[5 + index * 3] = 0;
+            dataWriter[6 + index * 3] = 0;
+        });
+    }
+
+    function identity() {
+        var identityMatrix = {};
+
+        identityMatrix.m11 = 1.0;
+        identityMatrix.m12 = 0.0;
+        identityMatrix.m13 = 0.0;
+        identityMatrix.m14 = 0.0;
+        identityMatrix.m21 = 0.0;
+        identityMatrix.m22 = 1.0;
+        identityMatrix.m23 = 0.0;
+        identityMatrix.m24 = 0.0;
+        identityMatrix.m31 = 0.0;
+        identityMatrix.m32 = 0.0;
+        identityMatrix.m33 = 1.0;
+        identityMatrix.m34 = 0.0;
+        identityMatrix.m41 = 0.0;
+        identityMatrix.m42 = 0.0;
+        identityMatrix.m43 = 0.0;
+        identityMatrix.m44 = 1.0;
+
+        return identityMatrix;
+    }
 
     function printHandler(args) {
-        var modelPackage = new Windows.Graphics.Printing3D.Printing3D3MFPackage();
+        var Printing3D = Windows.Graphics.Printing3D;
 
-        var stl = processor.currentObject.toStlString();
+        var modelPackage = new Printing3D.Printing3D3MFPackage();
 
-        console.log("STL: " + stl);
+        var model = new Printing3D.Printing3DModel();
 
-        args.setSource(modelPackage);
+        model.unit = Printing3D.Printing3DModelUnit.Millimeter;
+
+        var materialGroup = new Printing3D.Printing3DBaseMaterialGroup(0);
+        var material = new Printing3D.Printing3DBaseMaterial();
+
+        material.name = Printing3D.Printing3DBaseMaterial.Pla;
+
+        materialGroup.bases.append(material);
+
+        model.material.baseGroups.append(materialGroup);
+
+        var component = new Printing3D.Printing3DComponent();
+
+        var triangles = cadProcessor.currentObject.toTriangles();
+                
+        var mesh = getVertices(triangles);
+
+        getIndices(triangles, mesh);
+
+        model.meshes.append(mesh);
+
+        component.mesh = mesh;
+
+        model.components.append(component);
+
+        var componentWithMatrix = new Printing3D.Printing3DComponentWithMatrix();
+
+        componentWithMatrix.component = component;
+
+        componentWithMatrix.matrix = identity();
+
+        model.build.components.append(componentWithMatrix);
+
+        modelPackage.saveModelToPackageAsync(model).then(function() {
+            args.setSource(modelPackage);
+        });
     }
 
     function onPrintRequestCompleted(args) {
@@ -54,9 +168,10 @@
         MSApp.clearTemporaryWebDataAsync();
 
         var statusButtons = processor.statusbuttons;
-        processor = processor;
+        cadProcessor = processor;
 
         var printButton = document.createElement("button");
+        printButton.setAttribute("style", "position:absolute; left:5px; bottom:5px;");
         var icon = document.createElement("span");
         icon.setAttribute("style", "font-family: Segoe MDL2 Assets; font-size:24pt;");
         icon.appendChild(document.createTextNode(""));//"&#xE2F7;"));
@@ -64,6 +179,7 @@
         printButton.appendChild(document.createElement("br"));
         printButton.appendChild(document.createTextNode("Print"));
         printButton.onclick = print3D;
+
 
         statusButtons.appendChild(printButton);
     }
