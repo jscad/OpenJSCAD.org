6cbef88659c219d68db39424710d7bcfc051a134
Z3 Development <z3-dev@gfnews.net>
2015-12-16 12:04:00 +0900

    Enhanced the editor frame to slide in and out by clicking a tab

--- js/index.js
+++ js/index.js
@@ -26,15 +26,25 @@ $(document).ready(function() {
 
     $("#menu").height($(window).height());       // initial height
 
+    $('#editFrame').height($(window).height());
     $(window).resize(function() {                // adjust the relevant divs
         $("#menu").height($(window).height());
         $("#menuHandle").css({top: '45%'});
+        $("#editFrame").height($(window).height());
       });
     setTimeout( function(){$('#menu').css('left','-280px');},3000); // -- hide slide-menu after 3secs
 
     $('#menu').mouseleave(function() {
         $('#examples').css('height',0); $('#examples').hide();
         $('#options').css('height',0); $('#options').hide();
+      });
+
+    $("#editHandle").click(function() {
+        if ($("#editFrame").width() == 0) {
+          $("#editFrame").css("width",'40%');
+        } else {
+          $("#editFrame").css("width",'0px');
+        }
       });
 
   // -- Examples
