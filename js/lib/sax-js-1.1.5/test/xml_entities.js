63378ad969947510a4072590ed8729ee74c1d9bd
Z3 Development <z3-dev@gfnews.net>
2016-03-03 14:50:38 +0900

    Adding XML SAX parser library for SVG (and other) file conversions

---
+++ js/lib/sax-js-1.1.5/test/xml_entities.js
@@ -0,0 +1,11 @@
+require(__dirname).test({
+  opt: { strictEntities: true },
+  xml: '<r>&rfloor; ' +
+    '&spades; &copy; &rarr; &amp; ' +
+    '&lt; < <  <   < &gt; &real; &weierp; &euro;</r>',
+  expect: [
+    ['opentag', {'name': 'R', attributes: {}, isSelfClosing: false}],
+    ['text', '&rfloor; &spades; &copy; &rarr; & < < <  <   < > &real; &weierp; &euro;'],
+    ['closetag', 'R']
+  ]
+})
