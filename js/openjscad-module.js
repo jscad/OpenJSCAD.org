2342d2494da23f239ff5f88749a3415ed2400234
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-05 14:28:40 +0100

    feat(openjscad node module): added stub (empty!) for node.js module outlining basic api - also added basics for future unit tests

---
+++ js/openjscad-module.js
@@ -0,0 +1,25 @@
+/**
+ * compile openjscad code and generates intermediate representation
+ * @param  {Object} params the set of parameters to use
+ * @param  {String} source the openjscad script we want to compile
+ */
+function compile (params, source) {
+}
+
+/**
+ * generate output file from intermediate representation
+ * @param  {Object} ir the openjscad intermediate representation
+ * @param  {String} outputFormat the output file format
+ */
+function generateOutput (ir, outputFormat) {
+}
+
+/**
+ * converts from input file to output file
+ * @param  {Object} ir the openjscad intermediate representation
+ * @param  {String} outputFormat the output file format
+ */
+function convert (inputData, inputFormat, outputFormat) {
+}
+
+module.exports = { compile, generateOutput, convert }
