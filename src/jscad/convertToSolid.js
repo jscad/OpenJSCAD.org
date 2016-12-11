7a9e70015a1adb8c67c607200a62fea8416b3eae
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-11 23:33:17 +0100

    refactor(): split out conversionFormats & convertToSolid data/ functions

---
+++ src/jscad/convertToSolid.js
@@ -0,0 +1,24 @@
+export default function convertToSolid(objs) {
+  if (objs.length === undefined) {
+    if ((objs instanceof CAG) || (objs instanceof CSG)) {
+      var obj = objs
+      objs = [obj]
+    } else {
+      throw new Error('Cannot convert object (' + typeof (objs) + ') to solid')
+    }
+  }
+
+  var solid = null
+  for (var i = 0; i < objs.length; i++) {
+    var obj = objs[i]
+    if (obj instanceof CAG) {
+      obj = obj.extrude({offset: [0, 0, 0.1]}) // convert CAG to a thin solid CSG
+    }
+    if (solid !== null) {
+      solid = solid.unionForNonIntersecting(obj)
+    } else {
+      solid = obj
+    }
+  }
+  return solid
+}
