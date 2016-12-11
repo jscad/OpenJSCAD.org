429cb474876dbaacc66172d70d4b7b6db2efaa6d
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-12 00:16:06 +0100

    refactor(jscad): split out env

---
+++ src/jscad/env.js
@@ -0,0 +1,13 @@
+function env() {
+  var env = 'OpenJSCAD ' + version
+  if (typeof document !== 'undefined') {
+    var w = document.defaultView
+    env = env + ' [' + w.navigator.userAgent + ']'
+  } else {
+    if (typeof require == 'function') {
+      var os = require('os')
+      env = env + ' [' + os.type() + ':' + os.release() + ',' + os.platform() + ':' + os.arch() + ']'
+    }
+  }
+  console.log(env)
+}
