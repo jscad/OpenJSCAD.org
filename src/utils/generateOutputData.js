c6a4ec0452ad44d1a58a9dc5e2158e6d554ec7d5
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-15 12:28:33 +0100

    refactor(generateOutputData): updated module to use convertToBlob + some cleanup

--- src/utils/generateOutputData.js
+++ src/utils/generateOutputData.js
@@ -1,16 +1,6 @@
-/*const path = require('path')
-const lib = global.lib //FIXME: EEEK !! horrible
-const formatsPath = path.resolve(lib, './formats.js')
-const CSG = require(formatsPath).CSG // use the CSG with extended prototypes
-const CAG = require(formatsPath).CAG // use the CAG with extended prototypes
-const blobPath = path.resolve(lib, './Blob.js')
-const Blob = require(blobPath).Blob*/
-
-//these should be use instead, double check for CLI use
-const CSG = require('../formats').CSG // use the CSG with extended prototypes
-const CAG = require('../formats').CAG // use the CAG with extended prototypes
-const Blob = require('../Blob').Blob
+const CAG = require('../csg').CAG
 const evaluateSource = require('./evaluateSource')
+const convertToBlob = require('../io/convertToBlob')
 
 /**
  * generate output data from source
@@ -21,28 +11,14 @@ const evaluateSource = require('./evaluateSource')
  * @param {String} src the original source
  * @return the output data
  */
-function generateOutputData (modelingHelpers, meta, mainParams, outputFormat, src)
+function generateOutputData (modelingHelpers, mainParams, outputFormat, src)
 {
+  let objects
   if (outputFormat === 'jscad' || outputFormat === 'js') {
-    //  console.log(`Blob: type [${data.type}] size [${data.size}]`)
-    return new Blob([src], { type: 'application/javascript' })
+    objects = src
   } else {
-    const csgObject = evaluateSource(modelingHelpers, CAG, mainParams, src)
-    const outputFormatHandlers = {
-      'amf': () => csgObject.toAMFString(meta), // CSG to AMF
-      'stlb': () => csgObject.toStlBinary(), // CSG to STL BINARY
-      'stl': () => csgObject.toStlString(), // CSG to STL ASCII
-      'stla': () => csgObject.toStlString(), // CSG to STL ASCII
-      'dxf': () => csgObject.toDxf(), // CAG to DXF
-      'svg': () => csgObject.toSvg(), // CAG to SVG
-      'x3d': () => csgObject.toX3D(), // CSG to X3D Only possible via browsers
-      'json': () => csgObject.toJSON(), // CSG or CAG to JSON
-      undefined: () => {
-        console.log('ERROR: only jscad, stl, amf, dxf, svg or json as output format')
-        process.exit(1)
-      }
-    }
-    return outputFormatHandlers[outputFormat]()
+    objects = evaluateSource(modelingHelpers, CAG, mainParams, src)
   }
+  return convertToBlob(objects, {format: outputFormat, formatInfo: {convertCAG: true, convertCSG: true}})
 }
 module.exports = generateOutputData
