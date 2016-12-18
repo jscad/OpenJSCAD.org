94af248aecf73529fd92d591d7668ce688d9886d
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-18 23:07:23 +0100

    refactor(convertToSolid): minor variable renaming

--- src/jscad/convertToSolid.js
+++ src/jscad/convertToSolid.js
@@ -1,19 +1,19 @@
 import { CSG, CAG } from '../csg'
 
 // FIXME: is there not too much overlap with convertToBlob ?
-export default function convertToSolid (objs) {
-  if (objs.length === undefined) {
-    if ((objs instanceof CAG) || (objs instanceof CSG)) {
-      var obj = objs
-      objs = [obj]
+export default function convertToSolid (objects) {
+  if (objects.length === undefined) {
+    if ((objects instanceof CAG) || (objects instanceof CSG)) {
+      var obj = objects
+      objects = [obj]
     } else {
-      throw new Error('Cannot convert object (' + typeof (objs) + ') to solid')
+      throw new Error('Cannot convert object (' + typeof (objects) + ') to solid')
     }
   }
 
   var solid = null
-  for (var i = 0; i < objs.length; i++) {
-    let obj = objs[i]
+  for (var i = 0; i < objects.length; i++) {
+    let obj = objects[i]
     if (obj instanceof CAG) {
       obj = obj.extrude({offset: [0, 0, 0.1]}) // convert CAG to a thin solid CSG
     }
