e4834c7d6357c50891043a1ce11febb2853f2e3c
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-22 23:44:49 +0100

    fix(cli) : more cli tweaks to get global install working

--- src/launch-cli.js
+++ src/launch-cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 //this is merely a helper file to boostrap es6 modules based code without the need to transpile files
-require('babel-core/register')
+require('babel-core/register')({ignore:true , presets:['es2015']})
 require('./cli')
