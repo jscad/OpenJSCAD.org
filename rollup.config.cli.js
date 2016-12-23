3b1a24b171e542daf4f918014b8102a63ca9a568
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-23 21:45:17 +0100

    feat(build): cli & module versions now use rollup to generate small bundles - added needed rollup tools & config - added dependencies - added commands

---
+++ rollup.config.cli.js
@@ -0,0 +1,28 @@
+import buble from 'rollup-plugin-buble'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-post-replace'
+
+export default {
+  entry: 'src/cli.js',
+  dest: 'dist/cli.js',
+  format: 'cjs',
+  moduleName: 'openjscad',
+  sourceMap: 'inline',
+  plugins: [
+    buble(),
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      namedExports: {
+        'src/csg.js': [ 'CSG', 'CAG' ]
+      }
+    }),
+    replace({
+      '../../package.json': '../package.json',// fix path issues
+      "'use strict';": "#!/usr/bin/env node\n'use strict';"// add shebang at start of file
+    })
+  ]
+}
---
+++ rollup.config.module.js
@@ -0,0 +1,27 @@
+import buble from 'rollup-plugin-buble'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-post-replace'
+
+export default {
+  entry: 'src/module.js',
+  dest: 'dist/module.js',
+  format: 'cjs',
+  moduleName: 'openjscad',
+  sourceMap: 'inline',
+  plugins: [
+    buble(),
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    commonjs({
+      namedExports: {
+        'src/csg.js': [ 'CSG', 'CAG' ]
+      }
+    }),
+    replace({
+      '../../package.json': '../package.json'
+    })
+  ]
+}
