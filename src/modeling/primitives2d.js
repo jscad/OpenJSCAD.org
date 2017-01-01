55c51a6dca01fdda2780ebbdff0e4975172e2767
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-01 11:13:18 +0100

    refactor(primitives2d): cleaned up 2d primitives module

--- src/modeling/primitives2d.js
+++ src/modeling/primitives2d.js
@@ -1,57 +1,65 @@
-import {CAG} from '../csg'
+import { CAG } from '../csg'
 
 // -- 2D primitives (OpenSCAD like notion)
 
-export function square() {
-   var v = [1,1], off; var a = arguments, p = a[0];
-   if(p&&!p.size) v = [p,p];
-   if(p&&p.length) v = a[0], p = a[1];
-   if(p&&p.size&&p.size.length) v = p.size;
+export function square () {
+  let v = [1, 1]
+  let off
+  let a = arguments
+  let p = a[0]
 
-   off = [v[0]/2,v[1]/2];
-   if(p&&p.center==true) off = [0,0];
+  if (p && !p.size) v = [p, p]
+  if (p && p.length) v = a[0], p = a[1]
+  if (p && p.size && p.size.length) v = p.size
 
-   var o = CAG.rectangle({center:off,radius:[v[0]/2,v[1]/2]});
+  off = [v[0] / 2, v[1] / 2]
+  if (p && p.center === true) off = [0, 0]
 
-   return o;
+  var o = CAG.rectangle({center: off, radius: [v[0] / 2, v[1] / 2]})
+
+  return o
 }
 
-export function circle() {
-   var r = 1, off, fn = 32; var a = arguments, p = a[0];
-   if(p&&p.r) r = p.r;
-   if(p&&p.fn) fn = p.fn;
-   if(p&&!p.r&&!p.fn&&!p.center) r = p;
-   off = [r,r];
-   if(p&&p.center==true) { off = [0,0]; }
-   var o = CAG.circle({center:off,radius:r,resolution:fn});
-   return o;
+export function circle () {
+  let r = 1
+  let off
+  let fn = 32
+  let a = arguments
+  let p = a[0]
+  if (p && p.r) r = p.r
+  if (p && p.fn) fn = p.fn
+  if (p && !p.r && !p.fn && !p.center) r = p
+  off = [r, r]
+  if (p && p.center === true) { off = [0, 0] }
+  var o = CAG.circle({center: off, radius: r, resolution: fn})
+  return o
 }
 
-export function polygon(p) {  // array of po(ints) and pa(ths)
-   var points = new Array();
-   if(p.paths&&p.paths.length&&p.paths[0].length) {          // pa(th): [[0,1,2],[2,3,1]] (two paths)
-      for(var j=0; j<p.paths.length; j++) {
-         for(var i=0; i<p.paths[j].length; i++) {
-            points[i] = p.points[p.paths[j][i]];
-         }
+export function polygon (p) { // array of po(ints) and pa(ths)
+  var points = [ ]
+  if (p.paths && p.paths.length && p.paths[0].length) { // pa(th): [[0,1,2],[2,3,1]] (two paths)
+    for (var j = 0; j < p.paths.length; j++) {
+      for (var i = 0; i < p.paths[j].length; i++) {
+        points[i] = p.points[p.paths[j][i]]
       }
-   } else if(p.paths&&p.paths.length) {                 // pa(th): [0,1,2,3,4] (single path)
-      for(var i=0; i<p.paths.length; i++) {
-         points[i] = p.points[p.paths[i]];
-      }
-   } else {                               // pa(th) = po(ints)
-      if(p.length) {
-         points = p;
-      } else {
-         points = p.points;
-      }
-   }
-   return CAG.fromPoints(points);
+    }
+  } else if (p.paths && p.paths.length) { // pa(th): [0,1,2,3,4] (single path)
+    for (var i = 0; i < p.paths.length; i++) {
+      points[i] = p.points[p.paths[i]]
+    }
+  } else { // pa(th) = po(ints)
+    if (p.length) {
+      points = p
+    } else {
+      points = p.points
+    }
+  }
+  return CAG.fromPoints(points)
 }
 
-export function triangle() {         // -- new addition
-   var a = arguments;
-   if(a[0]&&a[0].length) a = a[0];
-   var o = CAG.fromPoints(a);
-   return o;
+export function triangle () { // -- new addition
+  var a = arguments
+  if (a[0] && a[0].length) a = a[0]
+  var o = CAG.fromPoints(a)
+  return o
 }
