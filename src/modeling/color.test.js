c48eaf42a9e0d2bd9d763c5c22220988ea6ff974
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-06 22:43:54 +0100

    test(color): added tests for color space conversion functions

--- src/modeling/color.test.js
+++ src/modeling/color.test.js
@@ -1,7 +1,35 @@
 import test from 'ava'
 import { cube, sphere } from './primitives3d'
 import { square, circle } from './primitives2d'
-import { color } from './color'
+import { color, rgb2hsl, hsl2rgb, rgb2hsv, hsv2rgb } from './color'
+
+test('rgb2hsl', t => {
+  const obs = rgb2hsl(1, 0, 0)
+  const expColor = [0, 1, 0.5]
+
+  t.deepEqual(obs, expColor)
+})
+
+test('hsl2rgb', t => {
+  const obs = hsl2rgb(0, 1, 0)
+  const expColor = [0, 0, 0]
+
+  t.deepEqual(obs, expColor)
+})
+
+test('rgb2hsv', t => {
+  const obs = rgb2hsv(1, 0, 0.5)
+  const expColor = [0.9166666666666666, 1, 1]
+
+  t.deepEqual(obs, expColor)
+})
+
+test('hsv2rgb', t => {
+  const obs = hsv2rgb(0, 0.2, 0)
+  const expColor = [0, 0, 0]
+
+  t.deepEqual(obs, expColor)
+})
 
 test('color (rgb, on 3d objects)', t => {
   const obs = color([1, 0, 0], cube(), sphere())
