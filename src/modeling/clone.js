f4e506a341c24791fbd2c625479c15b57dccd5f4
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-06 23:35:55 +0100

    refactor(clone): superficial clone cleanup

--- src/modeling/clone.js
+++ src/modeling/clone.js
@@ -1,5 +1,5 @@
 export function clone (obj) {
-  if (null == obj || 'object' != typeof obj) return obj
+  if (obj === null || typeof obj !== 'object') return obj
   var copy = obj.constructor()
   for (var attr in obj) {
     if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr]
