4682781e95ff90782d2d60748772969c9ede9372
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-04-03 17:14:52 +0200

    feat(walkFileTree): added support for basic Files (different d&drop data source) - restructure, renamed, cleaned up a good portion of the code

--- src/ui/dragDrop/walkFileTree.js
+++ src/ui/dragDrop/walkFileTree.js
@@ -1,4 +1,5 @@
 import { conversionFormats } from '../../jscad/conversionFormats'
+import {findMainFile, changedFiles} from './helpers'
 
 function flatten (array) {
   return [].concat(...array)
@@ -19,26 +20,32 @@ const readFileAsync = function (file, fileMeta) {
   })
 }
 
-function isOkFormat (file) {
+export function isSupportedFormat (file) {
   var e = file.name.toLowerCase().match(/\.(\w+)$/i)
   e = RegExp.$1
   return conversionFormats.indexOf(e) >= 0
+  // NOTE: was incrementing memFsTotal++ ONLY if format is valid
 }
 
-function pseuDoArraytoArray (pseudoArray) {
+export function pseudoArraytoArray (pseudoArray) {
   let array = []
   for (var i = 0; i < pseudoArray.length; i++) {
     array.push(pseudoArray[i])
   }
+  console.log('fooarray', array)
   return array
 }
 
 function processItems (items) {
-  let results = pseuDoArraytoArray(items).reduce((result, item) => {
+  let results = pseudoArraytoArray(items).reduce((result, item) => {
+    item = item.webkitGetAsEntry ? item.webkitGetAsEntry() : item
     if (item.isFile) {
       result.push(processFile(item))
     } else if (item.isDirectory) {
       result.push(processDirectory(item))
+    } else if (item instanceof File) {
+      const file = isSupportedFormat(item) ? readFileAsync(item, {fullPath:undefined}) : undefined
+      result.push(file)
     }
     return result
   }, [])
@@ -49,7 +56,7 @@ function processItems (items) {
 function processFile (fileItem) {
   return new Promise(function (resolve, reject) {
     fileItem.file(function (fileData) {
-      isOkFormat(fileData) ? resolve(readFileAsync(fileData, fileItem)) : resolve(undefined)
+      isSupportedFormat(fileData) ? resolve(readFileAsync(fileData, fileItem)) : resolve(undefined)
     }, reject)
   })
 }
@@ -68,9 +75,26 @@ function processDirectory (directory) {
 //    1) walk the tree
 //    2) read the files (readFileAsync)
 //    3) re-render if there was a change (via readFileAsync)
-export function walkFileTree (item, path) {
-  console.log('walkFileTree', item)
-  return processItems(item, path).then(function (result) {
-    console.log('results yeah', result)
-  })
+export function walkFileTree (items) {
+  return processItems(items)
+}
+
+export function afterFilesRead ({memFs, memFsCount, memFsTotal, memFsChanged}, files) {
+  memFsCount = files.length
+  memFsTotal = files.length
+
+  // FIXME : THIRD time the SAME data is cached
+  // saveScript(memFs, file.name, file.source)
+  const mainFile = findMainFile({memFs, memFsTotal}, files)
+
+  if (changedFiles(memFs, files).length > 0) {
+    if (!mainFile) throw new Error('No main.jscad found')
+    setCurrentFile(mainFile) // ARGH , cannot do this here
+  }
+}
+
+function afterReadFileASyncError () {
+  throw new Error('Failed to read file')
+  if (gProcessor) gProcessor.clearViewer()
+  previousScript = null
 }
