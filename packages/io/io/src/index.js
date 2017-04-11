98b09e60b147fa2c444b9bb68aac12b2228f84de
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-04-11 10:06:23 +0200

    refactor(): moved convertToBlob, worker etc back to main jscad repo

--- packages/io/io/src/index.js
+++ packages/io/io/src/index.js
@@ -1,4 +1,18 @@
-import convertToBlob from './convertToBlob'
-import createConversionWorker from './createConversionWorker'
-import {formats} from './formats'
-export {convertToBlob, createConversionWorker, formats}
+
+//import convertToBlob from './convertToBlob'
+//import createConversionWorker from './createConversionWorker'
+//import {formats} from './formats'
+import {revokeBlobUrl, makeBlob} from './utils/Blob'
+export {makeBlob, revokeBlobUrl}
+
+export {CAGToDxf} from './writers/CAGToDxf'
+export {CAGToJson} from './writers/CAGToJson'
+export {CAGToSvg} from './writers/CAGToSvg'
+export {CSGToAMF} from './writers/CSGToAMF'
+export {CSGToJson} from './writers/CSGToJson'
+export {CSGToStla} from './writers/CSGToStla'
+export {CSGToStlb} from './writers/CSGToStlb'
+/*const convertToBlob = require('./convertToBlob')
+module.exports = {
+  convertToBlob
+}*/
