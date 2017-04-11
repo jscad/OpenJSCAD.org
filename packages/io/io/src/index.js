78f5592abca491bf9c7554cf9f8a90152b080187
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-04-11 12:19:25 +0200

    feat(index): added re-exports of all parsers/writers

--- packages/io/io/src/index.js
+++ packages/io/io/src/index.js
@@ -1,18 +1,18 @@
-
-//import convertToBlob from './convertToBlob'
-//import createConversionWorker from './createConversionWorker'
-//import {formats} from './formats'
 import {revokeBlobUrl, makeBlob} from './utils/Blob'
 export {makeBlob, revokeBlobUrl}
 
-export {CAGToDxf} from './writers/CAGToDxf'
-export {CAGToJson} from './writers/CAGToJson'
-export {CAGToSvg} from './writers/CAGToSvg'
-export {CSGToAMF} from './writers/CSGToAMF'
-export {CSGToJson} from './writers/CSGToJson'
-export {CSGToStla} from './writers/CSGToStla'
-export {CSGToStlb} from './writers/CSGToStlb'
-/*const convertToBlob = require('./convertToBlob')
-module.exports = {
-  convertToBlob
-}*/
+export {default as CAGToDxf} from './writers/CAGToDxf'
+export {default as CAGToJson} from './writers/CAGToJson'
+export {default as CAGToSvg} from './writers/CAGToSvg'
+export {default as CSGToAMF} from './writers/CSGToAMF'
+export {default as CSGToJson} from './writers/CSGToJson'
+export {default as CSGToStla} from './writers/CSGToStla'
+export {default as CSGToStlb} from './writers/CSGToStlb'
+export {default as CSGToX3D} from './writers/CSGToX3D'
+
+export {parseAMF} from './parsers/parseAMF'
+export {parseGCode} from './parsers/parseGCode'
+export {parseJSON} from './parsers/parseJSON'
+export {parseOBJ} from './parsers/parseOBJ'
+export {parseSTL} from './parsers/parseSTL'
+export {parseSVG} from './parsers/parseSVG'
