ea58a8b73fb5b18421b3987bacb66d772f59964a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-12-11 20:28:40 +0100

    chore(imports): using included csg.js instead of dev path

--- packages/jscad-desktop/src/scripLoading.js
+++ packages/jscad-desktop/src/scripLoading.js
@@ -36,23 +36,23 @@ function requireUncached (module) {
   return require(module)
 }
 
-function loadScript (filePath, csgBasePath = './node_modules/@jscad/scad-api') {
+function loadScript (filePath, csgBasePath = './node_modules/@jscad') {
   const scriptAsText = fs.readFileSync(filePath, 'utf8')
   let jscadScript
   if (!scriptAsText.includes('module.exports') && scriptAsText.includes('main')) {
     const getParamsString = scriptAsText.includes('getParameterDefinitions')
       ? 'module.exports.getParameterDefinitions = getParameterDefinitions' : ''
     const commonJsScriptText = `
-    const {CSG, CAG} = require('../../core/csg.js/csg')
-    const {square, circle, polygon} = require('${csgBasePath}').primitives2d
-    const {cube, cylinder, sphere, polyhedron, torus} = require('${csgBasePath}').primitives3d
-    const {color} = require('${csgBasePath}').color
-    const {rectangular_extrude, linear_extrude, rotate_extrude} = require('${csgBasePath}').extrusions
-    const {rotate, translate, scale, hull, chain_hull} = require('${csgBasePath}').transformations
-    const {union, difference, intersection} = require('${csgBasePath}').booleanOps
-    const {sin, cos, tan, sqrt, lookup} = require('${csgBasePath}').maths
-    const {hsl2rgb} = require('${csgBasePath}').color
-    const {vector_text} = require('${csgBasePath}').text
+    const {CSG, CAG} = require('${csgBasePath}/csg')
+    const {square, circle, polygon} = require('${csgBasePath}/scad-api').primitives2d
+    const {cube, cylinder, sphere, polyhedron, torus} = require('${csgBasePath}/scad-api').primitives3d
+    const {color} = require('${csgBasePath}/scad-api').color
+    const {rectangular_extrude, linear_extrude, rotate_extrude} = require('${csgBasePath}/scad-api').extrusions
+    const {rotate, translate, scale, hull, chain_hull} = require('${csgBasePath}/scad-api').transformations
+    const {union, difference, intersection} = require('${csgBasePath}/scad-api').booleanOps
+    const {sin, cos, tan, sqrt, lookup} = require('${csgBasePath}/scad-api').maths
+    const {hsl2rgb} = require('${csgBasePath}/scad-api').color
+    const {vector_text} = require('${csgBasePath}/scad-api').text
     ${scriptAsText}
     module.exports = main
     ${getParamsString}
