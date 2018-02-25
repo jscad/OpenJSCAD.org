e42c9181fa421ff4870e2d5ef00b9b0efa1cc806
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-02-25 16:08:24 +0100

    refactor(writeOutput): merged the stl serialization pass into writeOutput

--- packages/io/jscad-tree-experiments/io/writeOutput.js
+++ packages/io/jscad-tree-experiments/io/writeOutput.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const {convertToBlob} = require('./convertToBlob')
+const serializer = require('@jscad/stl-serializer')
 
 const writeOutput = (filePath, data) => {
+  data = serializer.serialize(data, {binary: true})
   const dataToBlob = {data, mimeType: 'application/sla'}
   const blob = convertToBlob(dataToBlob)
   fs.writeFileSync(filePath, blob.asBuffer())
