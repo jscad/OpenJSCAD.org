cd12b0b02b2a679bc93bca5a164fd57b41f3e6ca
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-02-26 15:22:27 +0100

    feat(): added WORKING implementation of caching for root level objects ! ie when returning arrays of shapes from main(): huge performance gains !

--- packages/io/jscad-tree-experiments/user/index.js
+++ packages/io/jscad-tree-experiments/user/index.js
@@ -1,13 +1,66 @@
 const generate = require('../core/geometry-generator')
 const generateTree = require('../core/geometry-tree-generator')
-
+const findDisconnectedSubGraphs = require('../core/findDisconnectedSubGraphs')
 const writeOutput = require('../io/writeOutput')
 
-const runCompare = require('./runCompare')
-// runCompare('./examples/logo')
-// runCompare('./examples/basic')
-// runCompare('./examples/union')
-runCompare('./examples/shapes-array')
+const {runCompare, runVTreeTree} = require('./runCompare')
+/* runCompare('./examples/logo')
+runCompare('./examples/basic')
+runCompare('./examples/union')
+runCompare('./examples/shapes-array') */
+
+//
+const hash = require('object-hash')
+const lookup = {}
+const buildFinalResult = (subTrees) => {
+  const finalResult = []
+  subTrees.forEach(function (indy, index) {
+    const subTreeHash = hash(indy)
+    const foundData = lookup[subTreeHash]
+    if (foundData !== undefined) {
+      // console.log('foundData', foundData.subTree)
+      finalResult.push(foundData.geom)
+    } else {
+      const subTreeGeom = generate(indy)
+      lookup[subTreeHash] = subTreeGeom
+      finalResult.push(subTreeGeom)
+    }
+  })
+  return finalResult
+}
+
+let start = process.hrtime()
+console.log('start optimised, first iteration')
+let results = require('./examples/complex-vtree')()
+const subTrees = findDisconnectedSubGraphs(results)
+buildFinalResult(subTrees)
+let elapsed = process.hrtime(start)[1] / 1000000
+
+console.log('optimised time', elapsed, 'ms')
+
+console.log('start optimised, changed one of output of main() (array of csgs)')
+start = process.hrtime()
+let resultsChanged = require('./examples/complex-vtree-changed')()
+const subTreesChanged = findDisconnectedSubGraphs(resultsChanged)
+buildFinalResult(subTreesChanged)
+
+elapsed = process.hrtime(start)[1] / 1000000
+console.log('optimised time', elapsed, 'ms')
+
+// measure vanilla
+const decache = require('decache')
+decache('./examples/complex-base')
+require('./examples/complex-base')()
+
+console.log('start vanilla')
+start = process.hrtime()
+decache('./examples/complex-base')
+require('./examples/complex-base')()
+
+elapsed = process.hrtime(start)[1] / 1000000
+console.log('vanilla time', elapsed, 'ms')
+
+// let geomResults = generateTree(results)
 
 /* writeOutput('foo.stl', generate(results))
 console.log('output written')
