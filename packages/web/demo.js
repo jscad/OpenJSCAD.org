1c6cb2856aee3fb775dbcd1c03836e95da76cf8a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-04-20 23:31:10 +0200

    chore(demo): demo adjustments

--- packages/web/demo.js
+++ packages/web/demo.js
@@ -1,20 +1,16 @@
 const makeJscad = require('./src/index')
 
-const options = {}
-
 const rootEl = document.createElement('div')
 document.body.appendChild(rootEl)
-
-console.log('creating jscad instances')
 
 //
 const el1 = document.createElement('div')
 el1.className = 'jscad1'
-const jscadInst1 = makeJscad(el1, options)
+const jscadInst1 = makeJscad(el1, {name: 'jscad1'})
 rootEl.appendChild(el1)
 
 //
 const el2 = document.createElement('div')
 el2.className = 'jscad2'
-const jscadInst2 = makeJscad(el2, options)
+const jscadInst2 = makeJscad(el2, {name: 'jscad2'})
 rootEl.appendChild(el2)
