7cd7c1642c7fac7b4faa78913bdc0399a06ea2b6
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-05-01 18:03:29 +0200

    refactor(i18n): split out specialized function to seperate modules

--- packages/core/sideEffects/i18n/index.js
+++ packages/core/sideEffects/i18n/index.js
@@ -26,42 +26,13 @@ const initTranslations = (options) => {
   const {localesPath, translations, baseTranslation} = Object.assign({}, defaults, options)
   // adapted from https://gist.github.com/gmarcos87/565d57747b30e1755046002137228562
 
-  const genericFile = translations['en']
-  //   const path = require('path')
-  // const genericFile = require(path.join(localesPath, baseTranslation) + '.json')
-  // Load all translation in locales folder
-  // this works for desktop, not web
-
-  // translations[baseTranslation] = {}// genericFile
-  /* require('fs').readdirSync(localesPath).forEach((file) => {
-    if (file.match(/\.json$/) !== null && baseTranslation + '.json' !== file) {
-      let name = file.replace('.json', '')
-      translations[name] = require(path.join(localesPath, file))
-    }
-  }) */
-
-  // check translations
-  Object.keys(translations)
-    .forEach(lang => {
-      const translationsMissing = missing(genericFile, translations[lang])
-      const translationsSurplus = missing(translations[lang], genericFile)
-      // Print Output
-      // console.log('checking language', lang + '.json')
-      translationsMissing.map(x => console.log('   missing ' + x))
-      translationsSurplus.map(x => console.log('   extre ' + x))
-    })
+  // const genericFile = translations['en']
   return translations
-}
-
-const missing = (master, slave) => {
-  const slaveKeys = Object.keys(slave)
-  return Object.keys(master).filter(key => slaveKeys.indexOf(key) === -1)
 }
 
 const i18nImport = require('es2015-i18n-tag')
 const i18n = i18nImport.default
 const {i18nConfig} = i18nImport
-// i18n, { i18nConfig }
 
 const makei18nSideEffect = (options) => {
   const translationsCB = callBackToStream()
