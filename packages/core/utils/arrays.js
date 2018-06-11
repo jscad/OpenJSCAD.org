799123310f02a3972fbc47356516e1088b6032c0
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-06-11 19:43:01 +0200

    refactor(): moved out array helpers to core package

--- packages/core/utils/arrays.js
+++ packages/core/utils/arrays.js
@@ -1,8 +1,22 @@
 /* converts input data to array if it is not already an array */
-function toArray (data) {
+const toArray = data => {
   if (!data) return []
   if (data.constructor !== Array) return [data]
   return data
 }
 
-module.exports = {toArray}
+const head = (array) => {
+  if (array === undefined || null) {
+    return undefined
+  }
+  if (array.length === 0) {
+    return undefined
+  }
+  return array[0]
+}
+
+const flatten = list => list.reduce(
+  (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+)
+
+module.exports = {toArray, head, flatten}
