93ad89ab670d8fe3f4c336c1c1f34c0583264efb
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-06-20 10:25:55 +0200

    fix(languages): fixed setting languages

--- packages/web/src/ui/flow/languages.js
+++ packages/web/src/ui/flow/languages.js
@@ -39,17 +39,18 @@ const actions = ({sources}) => {
 
   // send a request to get the translations for the specificed language
   const requestGetLanguageData$ = sources.state
-    .map(state => state.locale)
-    .skipRepeats()
+    .filter(state => state.languages && state.languages.active !== undefined)
+    .map(state => state.languages.active)
+    .skipRepeatsWith((state, previousState) => JSON.stringify(state) === JSON.stringify(previousState))
     .map(data => ({type: 'changeSettings', data, sink: 'i18n'}))
+    .tap(x => console.log('requestGetLanguageData', x))
 
   // set the current language, either from defaults, previous settings or manually
   const setLanguage$ = most.mergeArray([
     sources.i18n
       .filter(reply => reply.type === 'getDefaultLocale')
       .map(reply => reply.data)
-      .delay(10)// to get stuff after the available ones have been set
-      ,
+      .delay(10),// to get stuff after the available ones have been set      
     sources.dom.select('#languageSwitcher').events('change')
       .map(e => e.target.value),
     sources.store
