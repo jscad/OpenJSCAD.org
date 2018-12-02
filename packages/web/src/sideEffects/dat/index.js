88dfa83cbae77eaef5d4efe365b84ad3a3523877
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2018-12-02 13:03:27 +0100

    feat(dat): added hackish support for dat archive versions * experimenting with 'watch'

--- packages/web/src/sideEffects/dat/index.js
+++ packages/web/src/sideEffects/dat/index.js
@@ -1,22 +1,6 @@
 const callBackToStream = require('@jscad/core/observable-utils/callbackToObservable')
 const makeLogger = require('../../utils/logger')
 
-/*
-  console.log('has dat', window.DatArchive)
-  const datUrl = 'dat://f29ffcb874e4a266a16d837136c320aad4e3b8f0e8c594c21f8b30fc2e5341c3#local-compare'
-  // 'dat://7077e324d1fe35e30ad829a03d79a78ce588ca0334a073774dfce15e3d55998b'
-  // 'dat://530a1c88246534dc6e1a97deda03f6ce3c463b189e7c2d5a4cdaf8f6c6a18e6f'// 'dat://f29ffcb874e4a266a16d837136c320aad4e3b8f0e8c594c21f8b30fc2e5341c3'
-  const archive = new DatArchive(datUrl, {})
-  archive.readdir('/', { recursive: true, stat: true }).then(filesAndFolders => {
-    console.log('filesAndFolders', filesAndFolders)
-    filesAndFolders.map(f => {
-      if (f.stat.isFile() && f.name.includes('js') && !f.name.includes('json')) {
-        archive.readFile(f.name).then(y => console.log(`fileContent for ${f.name} "${y}"`))
-      }
-      // f.stat.isFile() ? archive.readFile(f.name)? : f
-    })
-  }) */
-
 const makeDatSideEffect = async (params) => {
   const commandResponses = callBackToStream()
   const defaults = { logging: false }
@@ -25,6 +9,7 @@ const makeDatSideEffect = async (params) => {
   let enabled = true
 
   if (!window.DatArchive) {
+    // todo handle loading of package for node.js side if/ when needed
     log.error('Dat archives not supported in this environment!')
     enabled = false
   }
@@ -33,6 +18,8 @@ const makeDatSideEffect = async (params) => {
     if (!enabled) { // bail out if not available
       return
     }
+
+    let archive
     // every time a new command is recieved (observable)
     commands$.forEach(command => {
       const { type, id, data, urls, options, path } = command
@@ -44,7 +31,11 @@ const makeDatSideEffect = async (params) => {
       }
       const read = () => {
         urls.map(async url => {
-          const archive = new DatArchive(url, {})
+          url = url.replace(' ', '+')
+          // const decoded = unescape(url)
+          // const foo = url.replace(' ', '+')
+          // console.log(`url "${url}" VS "${decoded}" VS ${foo}`)
+          archive = new DatArchive(url, {})
           const filesAndFolders = await archive.readdir(path, { recursive: true, stat: true })
           // console.log('filesAndFolders', filesAndFolders)
           const fileContents = await Promise.all(filesAndFolders.map(async f => {
@@ -67,6 +58,17 @@ const makeDatSideEffect = async (params) => {
             name: rootFolder
           }]
           commandResponses.callback({ type, id, url, data: hiearchyRoot })
+          watch()
+        })
+      }
+
+      const watch = () => {
+        const evts = archive.watch()
+        console.log('starting watch')
+        evts.addEventListener('changed', async ({ path }) => {
+          console.log(path, 'has been updated!')
+          const content = await archive.readFile(path)
+          console.log('content', content)
         })
       }
 
