9aa9949068e857b102a3513a525f0747dd4db0fa
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2019-01-03 11:35:36 +0100

    chore(examples): added require-ing of stl file example

--- packages/examples/core/module-design/index.js
+++ packages/examples/core/module-design/index.js
@@ -1,14 +1,14 @@
 const mountPlate = require('./mountPlate.js')
 
-const getParameterDefinitions = () => {
+const getParameterDefinitions = (prevParamValues) => {
   return [
     { name: 'show', type: 'checkbox', checked: false, caption: 'Show:' },
-    { name: 'size', type: 'float', initial: 25, caption: 'size', min: 50, max: 200 }
+    { name: 'length', type: 'float', initial: 25, caption: 'length', min: 50, max: 200 }
   ]
 }
 
 const main = params => {
-  return mountPlate(params.size)
+  return mountPlate(params.length)
 }
 
 module.exports = { main, getParameterDefinitions }
