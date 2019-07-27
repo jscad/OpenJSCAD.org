66e3809f7c3b01cc7199adbd5abe11f1a496fb89
Z3 Development <z3-dev@gfnews.net>
2019-07-27 17:24:01 +0900

    enhance(web) added japanese locale, and adjusted other locales changed to context based keys phrases for locales, simplifying maintenance corrected view of options and shortcuts

--- packages/web/src/ui/views/options.js
+++ packages/web/src/ui/views/options.js
@@ -6,18 +6,19 @@ module.exports = function options (state, i18n) {
     return html`<option value='${language.code}' selected=${selected}>${i18n.translate(language.fullName)}</option>`
   })
   const shortcuts = require('./shortcuts')(state, i18n)
+
   return html`
-<section id='options' style='visibility:${state.activeTool === 'options' ? 'visible' : 'hidden'}; color:${state.themes.themeSettings.secondaryTextColor}'>   
-  <br>
+  <section id='options' style='visibility:${state.activeTool === 'options' ? 'visible' : 'hidden'}; color:${state.themes.themeSettings.secondaryTextColor}'>   
+
   <fieldset>
-    <legend> <h3> ${i18n`language`}</legend>
+    <legend> <h3> ${i18n`options.language.h3`} </h3> </legend>
     <select id='languageSwitcher'>
       ${languages}  
     </select>
   </fieldset>
 
   <fieldset>
-    <legend> <h3> ${i18n`theme`} </h3> </legend>
+    <legend> <h3> ${i18n`options.theme.h3`} </h3> </legend>
     <select id='themeSwitcher'>
       <option value='dark' selected=${state.themes.active === 'dark'}>dark</option>
       <option value='light' selected=${state.themes.active === 'light'}>light</option>
@@ -25,41 +26,40 @@ module.exports = function options (state, i18n) {
   </fieldset>
 
   <fieldset>
-    <legend> <h3> ${i18n`geometry generation`}</h3> </legend>
-    <label>${i18n`experimental geometry caching`} ${i18n`see`} <a href='https://github.com/jscad/jscad-desktop#geometry-caching' target="_blank">docs</a>
+    <legend> <h3> ${i18n`options.generation.h3`}</h3> </legend>
+    <label>${i18n`options.generation.caching`} ${i18n`see`} <a href='https://github.com/jscad/jscad-desktop#geometry-caching' target="_blank">docs</a>
       <input id='toggleVtreeMode' type='checkbox' checked=${state.design.vtreeMode}/>
     </label>
-    <label>${i18n`timeout for solids generation`}
+    <label>${i18n`options.generation.timeout`}
       <input id='solidsTimeout' type='number' min=0 max=200000 value=${state.design.solidsTimeOut} />
     </label>
   </fieldset>
 
-    <fieldset>
-      <legend> <h3> ${i18n`3d viewer`} </h3> </legend>  
-      <label>${i18n`zoom to fit on new parameters`}
-        <input type='checkbox' checked=false id='viewer-zoomtofitonparamschange' disabled />
-      </label>
-      <label>${i18n`zoom to fit on design load`}
-        <input type='checkbox' checked=false id='viewer-zoomtofitonload' disabled />
-      </label>
-    </fieldset>
+  <fieldset>
+    <legend> <h3> ${i18n`options.viewer.h3`} </h3> </legend>
+    <label>${i18n`options.viewer.zoomonparamchange`}
+      <input type='checkbox' checked=false id='viewer-zoomtofitonparamschange' disabled />
+    </label>
+    <label>${i18n`options.viewer.zoomonreload`}
+      <input type='checkbox' checked=false id='viewer-zoomtofitonload' disabled />
+    </label>
+  </fieldset>
 
-    <fieldset>
-      <legend> <h3> ${i18n`storage`} </h3> </legend>  
-      <label>${i18n`settings storage path`} ${i18n`(not settable)`}
-        <input type='text' disabled value='${state.storage.path}' disabled />
-      </label>
-    </fieldset>
+  <fieldset>
+    <legend> <h3> ${i18n`options.storage.h3`} </h3> </legend>
+    <label>${i18n`options.storage.settingspath`} ${i18n`notsettable`}
+      <input type='text' disabled value='${state.storage.path}' disabled />
+    </label>
+  </fieldset>
 
-    <section id='shortcuts'>   
-      <h3> ${i18n`file handling`} </h3>
-      <label>${i18n`convert supported types to jscad script (overwrite main())`}
-        <input type='checkbox' checked=false id='design-convertSupportedTypes' checked=${state.design.convertSupportedTypes}/>
-      </label>
-    </section>
+  <fieldset>
+    <legend> <h3> ${i18n`options.filehandling.h3`} </h3> </legend>
+    <label>${i18n`options.filehandling.converttoscript`}
+      <input type='checkbox' checked=false id='design-convertSupportedTypes' checked=${state.design.convertSupportedTypes}/>
+    </label>
+  </fieldset>
 
-    ${shortcuts}
+  ${shortcuts}
 
-    
 </section>`
 }
