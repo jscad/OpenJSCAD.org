838c1970a9f226343bcaff6f5c11bae7ac156aa9
Z3 Development <z3-dev@gfnews.net>
2019-07-31 14:30:26 +0900

    fix(web) corrected label 'for' settings in viewerControls

--- packages/web/src/ui/views/viewerControls.js
+++ packages/web/src/ui/views/viewerControls.js
@@ -2,11 +2,11 @@ const html = require('bel')
 
 module.exports = function viewerControls (state, i18n) {
   return html`<div id='controls'>
-  <label for="toggle grid">${i18n`grid`}</label>
+  <label for="grid">${i18n`grid`}</label>
     <input type="checkbox" id="grid" checked=${state.viewer.grid.show} />
-  <label for="toggle axis">${i18n`axes`}</label>
+  <label for="toggleAxis">${i18n`axes`}</label>
     <input type="checkbox" id="toggleAxes" checked=${state.viewer.axes.show} />
-  <label for="toggle auto rotate">${i18n`auto rotate`}</label>
+  <label for="autoRotate">${i18n`auto rotate`}</label>
     <input type="checkbox" id="autoRotate"/>
 </div>`
 }
