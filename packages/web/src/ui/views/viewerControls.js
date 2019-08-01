4bef39809ab738481a9354de057f13de31cb2e96
Z3 Development <z3-dev@gfnews.net>
2019-08-01 20:48:49 +0900

    feat(languages): Japanese Language Support (#456)
    * feat(core) added japanese to longNames of languages
    added code to use the 'language' from the locale file, if found
    * fix(web) added padding for keyboard shortcuts section
    * feat(web) added japanese locale, and adjusted other locales
    changed to context based keys phrases for locales, simplifying maintenance
    corrected view of options and shortcuts
    * fix(web) changed i18n keys to short english phrases
    * fix(web) updated locales to use new key phrases
    * fix(web) corrected label 'for' settings in viewerControls
    * fix(web) once again correct label 'for' in viewerControls

--- packages/web/src/ui/views/viewerControls.js
+++ packages/web/src/ui/views/viewerControls.js
@@ -6,7 +6,7 @@ module.exports = function viewerControls (state, i18n) {
     <input type="checkbox" id="grid" checked=${state.viewer.grid.show} />
   <label for="toggleAxes">${i18n`axes`}</label>
     <input type="checkbox" id="toggleAxes" checked=${state.viewer.axes.show} />
-  <label for="autoRotate">${i18n`autorotate`}</label>
+  <label for="autoRotate">${i18n`auto rotate`}</label>
     <input type="checkbox" id="autoRotate"/>
 </div>`
 }
