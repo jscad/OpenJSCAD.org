a5c23173c12dfc015e3dacb4b24c8e2bb4e649b1
Z3 Development <z3-dev@gfnews.net>
2019-09-06 08:31:54 +0900

    fix(amf-deserializer) split up tests into instantiate and translate suites

--- packages/io/amf-deserializer/tests/instantiate.test.js
+++ packages/io/amf-deserializer/tests/instantiate.test.js
@@ -4,32 +4,11 @@ const test = require('ava')
 
 const { geometry } = require('@jscad/csg')
 
-const deserializer = require('./index.js')
+const deserializer = require('../index.js')
 
 const samplesPath = path.dirname(require.resolve('@jscad/sample-files/package.json'))
 
 const toArray = (polygons) => polygons.map((p) => p.vertices.map((v) => ([v[0], v[1], v[2]])))
-
-const countOf = (search, string) => {
-  let count = 0
-  let index = string.indexOf(search)
-  while (index !== -1) {
-    count++
-    index = string.indexOf(search, index + 1)
-  }
-  return count
-}
-
-test('translate simple amf file to jscad code', function (t) {
-  const inputPath = path.resolve(samplesPath, 'amf/Amf_Cube.amf')
-  const inputFile = fs.readFileSync(inputPath)
-
-  const observed = deserializer.deserialize(inputFile, undefined, {output: 'jscad', addMetaData: false})
-console.log(observed)
-  t.is(countOf('poly3.fromPoints', observed), 12)
-  t.is(countOf('color.color', observed), 12)
-  t.is(countOf('geom3.create', observed), 1)
-})
 
 test('deserialize simple amf to geometry', function (t) {
   const inputPath = path.resolve(samplesPath, 'amf/Amf_Cube.amf')
