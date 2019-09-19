8af00ac1bbad06e7b6f4a6e338a06ee6f8a37b1e
Z3 Development <z3-dev@gfnews.net>
2019-09-19 18:02:31 +0900

    fix(stl-serialize) initial version 2 code with lots of changes

--- packages/io/stl-serializer/CSGToStlb.js
+++ packages/io/stl-serializer/CSGToStlb.js
@@ -1,9 +1,10 @@
+const { geometry } = require('@jscad/csg')
 
 // see http://en.wikipedia.org/wiki/STL_%28file_format%29#Binary_STL
 
-// objects must be an array of CSG objects (with polygons)
+// objects must be an array of 3D geometries
 const serializeBinary = (objects, options) => {
-  options.statusCallback && options.statusCallback({progress: 0})  
+  options.statusCallback && options.statusCallback({ progress: 0 })
 
   // first check if the host is little-endian:
   let buffer = new ArrayBuffer(4)
@@ -16,8 +17,9 @@ const serializeBinary = (objects, options) => {
 
   let numtriangles = 0
   let numpolygons = 0
-  objects.forEach(function (object, i) {
-    object.polygons.forEach(function (polygon) {
+  objects.forEach((object, i) => {
+    let polygons = geometry.geom3.toPolygons(object)
+    polygons.forEach((polygon) => {
       let numvertices = polygon.vertices.length
       let thisnumtriangles = (numvertices >= 3) ? numvertices - 2 : 0
       numtriangles += thisnumtriangles
@@ -50,21 +52,23 @@ const serializeBinary = (objects, options) => {
 
   let byteoffset = 0
   let p = 0
-  objects.forEach(function (object) {
-    object.polygons.forEach(function (polygon) {
-      let numvertices = polygon.vertices.length
+  objects.forEach((object) => {
+    let polygons = geometry.geom3.toPolygons(object)
+    polygons.forEach((polygon) => {
+      let vertices = polygon.vertices
+      let numvertices = vertices.length
+      let plane = polygon.plane
       for (let i = 0; i < numvertices - 2; i++) {
-        let normal = polygon.plane.normal
-        triangleFloat32array[0] = normal._x
-        triangleFloat32array[1] = normal._y
-        triangleFloat32array[2] = normal._z
+        triangleFloat32array[0] = plane[0]
+        triangleFloat32array[1] = plane[1]
+        triangleFloat32array[2] = plane[2]
         let arindex = 3
         for (let v = 0; v < 3; v++) {
           let vv = v + ((v > 0) ? i : 0)
-          let vertexpos = polygon.vertices[vv].pos
-          triangleFloat32array[arindex++] = vertexpos._x
-          triangleFloat32array[arindex++] = vertexpos._y
-          triangleFloat32array[arindex++] = vertexpos._z
+          let vertex = vertices[vv]
+          triangleFloat32array[arindex++] = vertex[0]
+          triangleFloat32array[arindex++] = vertex[1]
+          triangleFloat32array[arindex++] = vertex[2]
         }
         triangleUint16array[0] = 0
         // copy the triangle into allTrianglesBuffer:
@@ -72,10 +76,10 @@ const serializeBinary = (objects, options) => {
         byteoffset += 50
       }
       p += 1
-      options.statusCallback && options.statusCallback({progress: 100 * p / numpolygons})
+      options.statusCallback && options.statusCallback({ progress: 100 * p / numpolygons })
     })
   })
-  options.statusCallback && options.statusCallback({progress: 100})
+  options.statusCallback && options.statusCallback({ progress: 100 })
   return [headerarray.buffer, ar1.buffer, allTrianglesBuffer] // 'blobable array'
 }
 
--- packages/io/stl-serializer/index.js
+++ packages/io/stl-serializer/index.js
@@ -1,59 +1,43 @@
 /*
-JSCAD Object to STL Format Serialization
+JSCAD Geometry to STL Format Serialization
 
 ## License
 
-Copyright (c) 2018 JSCAD Organization https://github.com/jscad
+Copyright (c) 2018-2019 JSCAD Organization https://github.com/jscad
 
 All code released under MIT license
 
 Notes:
-1) CAG conversion to:
+1) geom2 conversion to:
      none
-2) CSG conversion to:
+2) geom3 conversion to:
      STL mesh
-3) Path2D conversion to:
+3) path2 conversion to:
      none
 */
 
-const {serializeBinary} = require('./CSGToStlb')
-const {serializeText} = require('./CSGToStla')
+const { geometry } = require('@jscad/csg')
 
-const {ensureManifoldness} = require('@jscad/io-utils')
-const {toArray} = require('@jscad/io-utils/arrays')
-const {isCSG} = require('@jscad/csg')
+const { serializeBinary } = require('./CSGToStlb')
+const { serializeText } = require('./CSGToStla')
 
 const mimeType = 'application/sla'
 
-const serialize = (...params) => {
-  let options = {}
-  let objects
-  if (params.length === 0) {
-    throw new Error('no arguments supplied to serialize function !')
-  } else if (params.length === 1) {
-    // assumed to be object(s)
-    objects = Array.isArray(params[0]) ? params[0] : params
-  } else if (params.length > 1) {
-    options = params[0]
-    objects = params[1]
-  }
-  // make sure we always deal with arrays of objects as inputs
-  objects = toArray(objects)
-
+const serialize = (options, ...objects) => {
   const defaults = {
     binary: true,
     statusCallback: null
   }
   options = Object.assign({}, defaults, options)
 
-  // only use valid CSG objects
-  let csgs = []
-  objects.forEach(function (object, i) {
-    if (isCSG(object) & object.polygons.length > 0) {
-      csgs.push(ensureManifoldness(object))
-    }
-  })
-  return options.binary ? serializeBinary(csgs, options) : serializeText(csgs, options)
+  // TBD flatten the objects
+
+  // convert only 3D geometries
+  objects = objects.filter((object) => geometry.geom3.isA(object))
+
+  if (objects.length === 0) throw new Error('the given objects cannot be converted')
+
+  return options.binary ? serializeBinary(objects, options) : serializeText(objects, options)
 }
 
 module.exports = {
