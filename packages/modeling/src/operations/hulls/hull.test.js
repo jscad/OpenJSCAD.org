f2f9215d9c9332f5189544c7a540de1f888b34cf
Z3 Development <z3-dev@gfnews.net>
2019-11-19 13:37:28 +0900

    task(hulls) added tests for 3D geometries

--- packages/modeling/src/operations/hulls/hull.test.js
+++ packages/modeling/src/operations/hulls/hull.test.js
@@ -1,8 +1,12 @@
 const test = require('ava')
 
-const { geom2, path2 } = require('../../geometry')
+const { geom2, geom3, path2 } = require('../../geometry')
+
+const { sphere, cuboid, ellipsoid } = require('../../primitives')
 
 const hull = require('./hull')
+
+const comparePolygonsAsPoints = require('../../../test/helpers/comparePolygonsAsPoints')
 
 test('hull (single, geom2)', t => {
   let geometry = geom2.create()
@@ -176,4 +180,72 @@ test('hull (multiple, various, path2)', t => {
   obs = hull(geometry5, geometry4)
   pts = path2.toPoints(obs)
   t.is(pts.length, 8)
+})
+
+test('hull (single, geom3)', t => {
+  let geometry = geom3.create()
+
+  let obs = hull(geometry)
+  let pts = geom3.toPoints(obs)
+
+  t.is(pts.length, 0)
+
+  geometry = sphere({ radius: 2, segments: 8 })
+
+  obs = hull(geometry)
+  pts = geom3.toPoints(obs)
+
+  t.is(pts.length, 32)
+})
+
+test('hull (multiple, geom3)', t => {
+  let geometry1 = cuboid({ size: [2, 2, 2] })
+
+  let obs = hull(geometry1, geometry1) // same
+  let pts = geom3.toPoints(obs)
+  let exp = [
+    [ [ -1, 1, -1 ], [ -1, 1, 1 ], [ 1, 1, 1 ], [ 1, 1, -1 ] ],
+    [ [ -1, 1, -1 ], [ 1, 1, -1 ], [ 1, -1, -1 ], [ -1, -1, -1 ] ],
+    [ [ -1, 1, -1 ], [ -1, -1, -1 ], [ -1, -1, 1 ], [ -1, 1, 1 ] ],
+    [ [ 1, -1, 1 ], [ 1, -1, -1 ], [ 1, 1, -1 ], [ 1, 1, 1 ] ],
+    [ [ 1, -1, 1 ], [ 1, 1, 1 ], [ -1, 1, 1 ], [ -1, -1, 1 ] ],
+    [ [ 1, -1, 1 ], [ -1, -1, 1 ], [ -1, -1, -1 ], [ 1, -1, -1 ] ]
+  ]
+
+  t.is(pts.length, 6)
+  t.true(comparePolygonsAsPoints(pts, exp))
+
+  let geometry2 = cuboid({ size: [3, 3, 3], center: [5, 5, 5]})
+
+  obs = hull(geometry1, geometry2)
+  pts = geom3.toPoints(obs)
+  exp = [
+    [ [ 1, -1, -1 ], [ 6.5, 3.5, 3.5 ], [ 6.5, 3.5, 6.5 ], [ 1, -1, 1 ] ],
+    [ [ -1, -1, 1 ], [ -1, -1, -1 ], [ 1, -1, -1 ], [ 1, -1, 1 ] ],
+    [ [ -1, -1, 1 ], [ 1, -1, 1 ], [ 6.5, 3.5, 6.5 ], [ 3.5, 3.5, 6.5 ] ],
+    [ [ -1, -1, 1 ], [ 3.5, 3.5, 6.5 ], [ 3.5, 6.5, 6.5 ], [ -1, 1, 1 ] ],
+    [ [ -1, 1, -1 ], [ -1, 1, 1 ], [ 3.5, 6.5, 6.5 ], [ 3.5, 6.5, 3.5 ] ],
+    [ [ -1, 1, -1 ], [ -1, -1, -1 ], [ -1, -1, 1 ], [ -1, 1, 1 ] ],
+    [ [ 6.5, 6.5, 6.5 ], [ 6.5, 3.5, 6.5 ], [ 6.5, 3.5, 3.5 ], [ 6.5, 6.5, 3.5 ] ],
+    [ [ 6.5, 6.5, 6.5 ], [ 6.5, 6.5, 3.5 ], [ 3.5, 6.5, 3.5 ], [ 3.5, 6.5, 6.5 ] ],
+    [ [ 6.5, 6.5, 6.5 ], [ 3.5, 6.5, 6.5 ], [ 3.5, 3.5, 6.5 ], [ 6.5, 3.5, 6.5 ] ],
+    [ [ 1, 1, -1 ], [ 1, -1, -1 ], [ -1, -1, -1 ], [ -1, 1, -1 ] ],
+    [ [ 1, 1, -1 ], [ -1, 1, -1 ], [ 3.5, 6.5, 3.5 ], [ 6.5, 6.5, 3.5 ] ],
+    [ [ 1, 1, -1 ], [ 6.5, 6.5, 3.5 ], [ 6.5, 3.5, 3.5 ], [ 1, -1, -1 ] ]
+  ]
+
+  t.is(pts.length, 12)
+  t.true(comparePolygonsAsPoints(pts, exp))
+})
+
+
+test('hull (multiple, overlapping, geom3)', t => {
+  let geometry1 = ellipsoid({ radius: [2, 2, 2] })
+  let geometry2 = ellipsoid({ radius: [3, 3, 3], center: [3, -3, 3]})
+  let geometry3 = ellipsoid({ radius: [3, 3, 3], center: [-3, -3, -3]})
+
+  let obs = hull(geometry1, geometry2, geometry3)
+  let pts = geom3.toPoints(obs)
+
+  t.is(pts.length, 160)
 })
