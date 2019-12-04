92eb1a8017dacb508eff46710b870849251e225a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2019-12-04 23:38:47 +0100

    chore(core): added missing import

--- packages/core/code-loading/modulifySource.js
+++ packages/core/code-loading/modulifySource.js
@@ -6,6 +6,8 @@ const modulifySource = (source, apiMainPath) => {
   const getParamsString = source.includes('getParameterDefinitions')
     ? 'module.exports.getParameterDefinitions = getParameterDefinitions' : ''
   const updatedSource = `
+    const {geom2, geom3, path2, pol2, poly3} = require('${apiMainPath}').geometry
+
     const {square, circle, polygon, rectangle, ellipse, roundedRectangle} = require('${apiMainPath}').primitives
     const {cube, cylinder, sphere, polyhedron, torus} = require('${apiMainPath}').primitives
     const {color, hsl2rgb} = require('${apiMainPath}').color
