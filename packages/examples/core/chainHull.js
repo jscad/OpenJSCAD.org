68ffe49f2275f5bbd815ab8bed706d0a7595cd55
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2019-12-05 14:01:29 +0100

    refactor(examples): overhauled chainHull example (first pass)

--- packages/examples/core/chainHull.js
+++ packages/examples/core/chainHull.js
@@ -1,34 +1,58 @@
-// title      : Chain Hull
-// author     : Rene K. Mueller
+/* title      : Chain Hull
+// author     : Rene K. Mueller, Moissette Mark
 // license    : MIT License
 // date       : 2013/04/18
 // description: Whosa whatsis suggested "Chain Hull" as described at
 //    https://plus.google.com/u/0/105535247347788377245/posts/aZGXKFX1ACN
+*/
 
-function main () {
-  var o = [];
-  var p = [];
+const { sin, cos } = Math
+const { circle } = require('@jscad/modeling').primitives
+const { translate, scale } = require('@jscad/modeling').transforms
+const { union } = require('@jscad/modeling').booleans
+const { hullChain } = require('@jscad/modeling').hulls
+const { extrudeLinear } = require('@jscad/modeling').extrusions
 
-  for (var i = 0; i < 12; i++) { // -- shell like
-    var x = sin(i / 12 * 180) * 10;
-    var y = cos(i / 12 * 180) * 10;
-    o.push(circle({center: true}).scale(6 - i / 2).translate([x, y, 0]));
+const main = () => {
+  const shell = []
+  const hexagon = []
+
+  for (let i = 0; i < 12; i++) { // -- shell like
+    const x = sin(i / 12 * 180) * 10
+    const y = cos(i / 12 * 180) * 10
+    shell.push(
+      translate([x, y, 0], scale(6 - i / 2, circle({ center: true })))
+    )
   }
 
-  var n = 6;
-  for (i = 0; i < n; i++) { // -- hexagon chain hulled
-    x = sin(i / n * 360) * 10;
-    y = cos(i / n * 360) * 10;
-    p.push(circle({center: true}).translate([x, y, 0]));
+  const n = 6
+  for (let i = 0; i < n; i++) { // -- hexagon chain hulled
+    const x = sin(i / n * 360) * 10
+    const y = cos(i / n * 360) * 10
+    hexagon.push(
+      translate([x, y, 0], circle({ center: true }))
+    )
   }
 
   return [
-    linear_extrude({height: 5}, chain_hull(o)).translate([-20, 0, 0]),
-    chain_hull(o),
-    union(o).translate([20, 0, 0]),
+    translate([-20, 0, 0],
+      extrudeLinear({ height: 5 }, hullChain(shell))
+    ),
+    hullChain(shell),
+    translate([20, 0, 0],
+      union(shell)
+    ),
 
-    linear_extrude({height: 5}, chain_hull({closed: true}, p)).translate([-25, 40, 0]),
-    chain_hull({closed: true}, p).translate([0, 40, 0]),
-    union(p).translate([25, 40, 0])
-  ];
+    translate([-25, 40, 0],
+      extrudeLinear({ height: 5 }, hullChain({ closed: true }, hexagon))
+    ),
+    translate([0, 40, 0],
+      hullChain({ closed: true }, hexagon)
+    ),
+    translate([25, 40, 0],
+      union(hexagon)
+    )
+  ]
 }
+
+module.exports = { main }
