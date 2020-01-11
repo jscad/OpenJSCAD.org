e54cdcde633608f838c81db9b2640c72a8c0bf0d
Z3 Development <z3-dev@gfnews.net>
2020-01-11 10:49:22 +0900

    chore(core) completed list of formats added helper functions; supportedInputExtensions, supportedOutputExtensions, supportedOutputFormats,

--- packages/core/io/formats.js
+++ packages/core/io/formats.js
@@ -8,77 +8,97 @@ const formats = {
     description: 'STereoLithography, Binary',
     extension: 'stl',
     mimetype: 'application/sla',
-    convertGeom3: true,
-    convertGeom2: false
-  },
+    deserializable: true,
+    convertGeom3: false,
+    convertGeom2: false },
   stla: {
     displayName: 'STL (ASCII)',
     description: 'STereoLithography, ASCII',
     extension: 'stl',
     mimetype: 'application/sla',
+    deserializable: false, // just once
     convertGeom3: true,
-    convertGeom2: false
-  },
+    convertGeom2: false },
   stlb: {
     displayName: 'STL (Binary)',
     description: 'STereoLithography, Binary',
     extension: 'stl',
     mimetype: 'application/sla',
+    deserializable: false, // just once
     convertGeom3: true,
-    convertGeom2: false
-  },
-
+    convertGeom2: false },
   amf: {
     displayName: 'AMF (experimental)',
     description: 'Additive Manufacturing File Format',
     extension: 'amf',
     mimetype: 'application/amf+xml',
-    convertGeom3: true,
-    convertGeom2: false },
-  x3d: {
-    displayName: 'X3D',
-    description: 'X3D File Format',
-    extension: 'x3d',
-    mimetype: 'model/x3d+xml',
+    deserializable: true,
     convertGeom3: true,
     convertGeom2: false },
   dxf: {
-    displayName: 'DXF',
+    displayName: 'DXF (ASCII)',
     description: 'AutoCAD Drawing Exchange Format',
     extension: 'dxf',
     mimetype: 'application/dxf',
+    deserializable: true,
     convertGeom3: true,
     convertGeom2: true },
+  gcode: {
+    displayName: 'GCODE',
+    description: 'G Programming Language File Format',
+    extension: 'gcode',
+    mimetype: 'application/octet-stream',
+    deserializable: false,
+    convertGeom3: false,
+    convertGeom2: false },
   jscad: {
     displayName: 'JSCAD',
     description: 'OpenJSCAD.org Source',
     extension: 'jscad',
     mimetype: 'application/javascript',
+    deserializable: true,
     convertGeom3: true,
-    convertGeom2: true
-  },
+    convertGeom2: true },
   js: {
     displayName: 'js',
     description: 'JavaScript Source',
     extension: 'js',
     mimetype: 'application/javascript',
+    deserializable: true,
     convertGeom3: true,
-    convertGeom2: true
-  },
+    convertGeom2: true },
+  obj: {
+    displayName: 'OBJ',
+    description: 'Wavefromt OBJ File Format',
+    extension: 'obj',
+    mimetype: 'text/plain',
+    deserializable: true,
+    convertGeom3: false,
+    convertGeom2: false },
   svg: {
     displayName: 'SVG',
     description: 'Scalable Vector Graphics Format',
     extension: 'svg',
     mimetype: 'image/svg+xml',
+    deserializable: true,
     convertGeom3: false,
-    convertGeom2: true
-  },
-  gcode: {
-    displayName: 'gcode',
-    description: 'G Programming Language File Format' },
+    convertGeom2: true },
+  x3d: {
+    displayName: 'X3D',
+    description: 'X3D File Format',
+    extension: 'x3d',
+    mimetype: 'model/x3d+xml',
+    deserializable: false,
+    convertGeom3: true,
+    convertGeom2: false },
   json: {
     displayName: 'json',
-    description: 'JavaScript Object Notation Format' }
+    description: 'JavaScript Object Notation Format',
+    extension: 'json',
+    mimetype: 'application/json',
+    deserializable: false,
+    convertGeom3: false,
+    convertGeom2: false }
 }
 
 // handled input formats that can be converted to jscad code
@@ -99,6 +119,7 @@ const conversionFormats = [
 ]
 
 // formats that can be inported
+// FIXME unused and not exported
 const supportedInputFormats = [
   // 3D file formats
   {
@@ -205,8 +226,47 @@ const supportedFormatsForObjects = objects => {
   return objectFormats
 }
 
+// Return a list of extensions as used by the serializers
+const supportedOutputExtensions = () => {
+  let supported = []
+  for (let format in formats) {
+    if (formats[format].convertGeom3 === true || formats[format].convertGeom2 === true) {
+      if (supported.indexOf(formats[format].extension) < 0) {
+        supported.push(formats[format].extension)
+      }
+    }
+  }
+  return supported
+}
+
+// Return a list of formats as used by the serializers
+const supportedOutputFormats = () => {
+  let supported = []
+  for (let format in formats) {
+    if (formats[format].convertGeom3 === true || formats[format].convertGeom2 === true) {
+      supported.push(format)
+    }
+  }
+  return supported
+}
+
+// Return a list of file extensions as used by the deserializers
+// See also code-loading/transfromSources.js
+const supportedInputExtensions = () => {
+  let supported = []
+  for (let format in formats) {
+    if (formats[format].deserializable === true) {
+      supported.push(formats[format].extension)
+    }
+  }
+  return supported
+}
+
 module.exports = {
   formats,
   conversionFormats,
+  supportedInputExtensions,
+  supportedOutputExtensions,
+  supportedOutputFormats,
   supportedFormatsForObjects
 }
