31b8563e6bae58bbba5a2b9b93a0c412af9d7e23
Z3 Development <z3-dev@gfnews.net>
2020-03-17 11:26:52 +0900

    fix(booleans) changed to calculate dynamic EPS, and adjust the precision of 2D geometries

--- packages/modeling/src/operations/booleans/intersectGeom2.js
+++ packages/modeling/src/operations/booleans/intersectGeom2.js
@@ -6,6 +6,8 @@ const fromFakePolygons = require('./fromFakePolygons')
 const to3DWalls = require('./to3DWalls')
 const intersectGeom3 = require('./intersectGeom3')
 
+const measureEpsilon = require('./measureEpsilon')
+
 /*
  * Return a new 2D geometry representing space in both the first geometry and
  * in the subsequent geometries. None of the given geometries are modified.
@@ -17,8 +19,9 @@ const intersect = (...geometries) => {
   const newgeometries = geometries.map((geometry) => to3DWalls({ z0: -1, z1: 1 }, geometry))
 
   let newgeom3 = intersectGeom3(newgeometries)
+  let epsilon = measureEpsilon(newgeom3)
 
-  return fromFakePolygons(geom3.toPolygons(newgeom3))
+  return fromFakePolygons(epsilon, geom3.toPolygons(newgeom3))
 }
 
 module.exports = intersect
--- packages/modeling/src/operations/booleans/subtractGeom2.js
+++ packages/modeling/src/operations/booleans/subtractGeom2.js
@@ -6,6 +6,8 @@ const fromFakePolygons = require('./fromFakePolygons')
 const to3DWalls = require('./to3DWalls')
 const subtractGeom3 = require('./subtractGeom3')
 
+const measureEpsilon = require('./measureEpsilon')
+
 /*
  * Return a new 2D geometry representing space in the first geometry but
  * not in the subsequent geometries. None of the given geometries are modified.
@@ -17,8 +19,9 @@ const subtract = (...geometries) => {
   const newgeometries = geometries.map((geometry) => to3DWalls({ z0: -1, z1: 1 }, geometry))
 
   let newgeom3 = subtractGeom3(newgeometries)
+  let epsilon = measureEpsilon(newgeom3)
 
-  return fromFakePolygons(geom3.toPolygons(newgeom3))
+  return fromFakePolygons(epsilon, geom3.toPolygons(newgeom3))
 }
 
 module.exports = subtract
