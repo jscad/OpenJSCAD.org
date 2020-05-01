8e21da029ea5e5f609ebab321e0038ad691b6ce0
Timothy Johnson <timothy@idioticdev.com>
2020-04-30 23:55:15 -0700

    typo

--- packages/web/src/utils/urlUtils.js
+++ packages/web/src/utils/urlUtils.js
@@ -1,13 +1,13 @@
 const url = require('url')
 
 const fetchUriParams = (uri, paramName, defaultValue = undefined) => {
-  const params = new url.URL(uri, true)
+  const params = new url.URL(uri)
   const result = params.searchParams.get(paramName)
   return result !== undefined ? result : defaultValue
 }
 
 const getAllUriParams = uri => {
-  return Object.fromEntries(new url.URL(uri, true).searchParams.entries())
+  return Object.fromEntries(new url.URL(uri).searchParams.entries())
 }
 
 module.exports = { fetchUriParams, getAllUriParams }
