290619621fbb0852e6f973529d506be596b653a3
Z3 Development <z3-dev@gfnews.net>
2020-06-24 14:28:52 +0900

    fix(web) corrected issues with export format selection, and export of selection

--- packages/web/src/ui/views/io.js
+++ packages/web/src/ui/views/io.js
@@ -2,14 +2,16 @@ const html = require('bel')
 
 module.exports = function io (state, i18n) {
   const formatsList = state.io.availableExportFormats
-    .map(function ({ name, displayName }) {
+    .map(({ name, displayName }) => {
       displayName = i18n.translate(displayName)
-      const selected = state.exportForma !== undefined ? state.exportFormat.toLowerCase() === name.toLowerCase() : undefined
+      const selected = state.io.exportFormat ? state.io.exportFormat.toLowerCase() === name.toLowerCase() : undefined
       return html`<option value=${name} selected='${selected}'>
       ${displayName}
     </option>`
     })
+
   const exportAvailable = state.io.availableExportFormats.length > 0
+
   return html`
   <section id='io'>
   
