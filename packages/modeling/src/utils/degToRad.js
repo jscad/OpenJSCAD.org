20371656607012dd779561371941bba5c9bf8311
Z3 Development <z3-dev@gfnews.net>
2020-07-29 14:29:22 +0900

    docs(modeling) added doc strings for degToRad and radToDeg

--- packages/modeling/src/utils/degToRad.js
+++ packages/modeling/src/utils/degToRad.js
@@ -1,4 +1,9 @@
-// radians = degrees * PI / 180
+/**
+ * Convert the given angle (degrees) to radians.
+ * @param {Number} degrees - angle in degrees
+ * @returns {Number} angle in radians
+ * @alias module:modeling/utils.degToRad
+ */
 const degToRad = (degrees) => degrees * 0.017453292519943295
 
 module.exports = degToRad
--- packages/modeling/src/utils/radToDeg.js
+++ packages/modeling/src/utils/radToDeg.js
@@ -1,4 +1,9 @@
-// degrees = radians * 180 / PI
+/**
+ * Convert the given angle (radians) to degrees.
+ * @param {Number} radians - angle in radians
+ * @returns {Number} angle in degrees
+ * @alias module:modeling/utils.radToDeg
+ */
 const radToDeg = (radians) => radians * 57.29577951308232
 
 module.exports = radToDeg
