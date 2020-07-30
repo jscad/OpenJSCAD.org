219088967bd83267d3d00f2223d91b041dcb1f5a
Clark, Simon <sclark@netsuite.com>
2020-07-30 18:19:29 -0400

    Adding bezier object property definitions to docs

--- packages/modeling/src/curves/bezier/create.js
+++ packages/modeling/src/curves/bezier/create.js
@@ -1,3 +1,21 @@
+/**
+ * Represents a bezier easing function.
+ * @typedef {Object} bezier
+ * @property {Array} points - The control points for the bezier curve. The first and last point will also be the start and end of the curve
+ * @property {string} pointType - A reference to the type and dimensionality of the points that the curve was created from
+ * @property {number} dimensions - The dimensionality of the bezier
+ * @property {Array} permutations - A pre-calculation of the bezier algorithm's co-efficients
+ * @property {Array} tangentPermutations - A pre-calculation of the bezier algorithm's tangent co-efficients
+ *
+ * @example
+ * const b = bezier.create([0,10]) // a linear progression from 0 to 10
+ * const b = bezier.create([0, 0, 10, 10]) // a symmetrical cubic easing curve that starts slowly and ends slowly from 0 to 10
+ * const b = bezier.create([0,0,0], [0,5,10], [10,0,-5], [10,10,10]]) // a cubic 3 dimensional easing curve that can generate position arrays for modelling
+ * Usage:
+ * let position = bezier.valueAt(t,b) // where 0 < t < 1
+ * let tangent = bezier.tangentAt(t,b) // where 0 < t < 1
+ *
+ */
 
 /**
  * Creates an object representing a bezier easing curve. Can have both an arbitrary number of control points, and an arbitrary number of dimensions.
--- packages/modeling/src/curves/bezier/index.js
+++ packages/modeling/src/curves/bezier/index.js
@@ -1,19 +1,7 @@
 /**
- * @module modeling/curves/bezier
- */
-
-/**
  * Represents a bezier easing function.
- *
- * @example
- * const b = bezier.create([0,10]) // a linear progression from 0 to 10
- * const b = bezier.create([0, 0, 10, 10]) // a symmetrical cubic easing curve that starts slowly and ends slowly from 0 to 10
- * const b = bezier.create([0,0,0], [0,5,10], [10,0,-5], [10,10,10]]) // a cubic 3 dimensional easing curve that can generate position arrays for modelling
- * Usage:
- * let position = bezier.valueAt(t,b) // where 0 < t < 1
- * let tangent = bezier.tangentAt(t,b) // where 0 < t < 1
- *
- * @typedef {Object} bezier
+ * @see {@link bezier} for data structure information.
+ * @module modeling/curves/bezier
  */
 module.exports = {
   create: require('./create'),
