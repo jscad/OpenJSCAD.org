f64fee0da7863a168ca7e58adb0736a95dd4a1bb
Clark, Simon <sclark@netsuite.com>
2020-08-05 11:32:23 -0400

    Updating color examples.

---
+++ packages/examples/core/color/colorCube.js
@@ -0,0 +1,58 @@
+/*
+// title      : ColorCube
+// author     : Simon Clark
+// license    : MIT License
+// description: looking at color functions
+// tags       : RGB, HSL, HSV
+// file       : colorCube.js
+*/
+
+const jscad = require('@jscad/modeling')
+const { colorize, hslToRgb, hsvToRgb } = jscad.colors
+const { cuboid } = jscad.primitives
+const { translate } = jscad.transforms
+
+const getTranslation = (x, y, z, steps) => {
+  const spacing = 4
+  return [
+    (x - steps / 2) * spacing,
+    (y - steps / 2) * spacing,
+    (z - steps / 2) * spacing
+  ]
+}
+
+const getColor = (a, b, c, method) => {
+  if (method === 'hsl') return hslToRgb(a, b, c)
+  else if (method === 'hsv') return hsvToRgb(a, b, c)
+  else return [a, b, c]
+}
+
+const main = (params) => {
+  const o = []
+  const rows = 8
+  for (let ix = 0; ix <= rows; ix++) {
+    for (let iy = 0; iy <= rows; iy++) {
+      for (let iz = 0; iz <= rows; iz++) {
+        const cube = translate(getTranslation(ix, iy, iz, rows), cuboid([1, 1, 1]))
+        const color = getColor(ix / rows, iy / rows, iz / rows, params.method)
+        o.push(colorize(color, cube))
+      }
+    }
+  }
+  return o
+}
+
+const getParameterDefinitions = () => {
+  return [
+    {
+      name: 'method',
+      type: 'choice',
+      caption: 'Colorize Method',
+      values: ['rgb', 'hsv', 'hsl'],
+      captions: ['(r,g,b) = (x,y,z)', '(h,s,v) = (x,y,z)', '(h,s,l) = (x,y,z)'],
+      initial: 0
+    }
+  ]
+}
+
+module.exports = { main, getParameterDefinitions }
--- packages/examples/core/color/transparency.js
+++ packages/examples/core/color/transparency.js
@@ -1,29 +1,29 @@
 /*
 // title      : Transparency
-// authors     : Rene K. Mueller, Moissette Mark
+// authors    : Rene K. Mueller, Moissette Mark
 // license    : MIT License
 // description: showing transparent objects
-// tags       : colors, transparency, hsl3rgb
+// tags       : colors, transparency, hslToRgb
 */
 
-const { color, hsl2rgb } = require('@jscad/modeling').color
+const { colorize, hslToRgb, colorNameToRgb } = require('@jscad/modeling').colors
 const { cuboid, cylinder } = require('@jscad/modeling').primitives
 const { translate } = require('@jscad/modeling').transforms
 
 const main = () => {
   const shapes = []
   for (let i = 7; i >= 0; i--) { // reverse order for seeing through all cylinders (see http://www.opengl.org/wiki/Transparency_Sorting)
-    // hsl to rgb, creating rainbow [r,g,b]
-    const shapeColor = hsl2rgb(i / 8, 1, 0.5).concat(1 / 8 + i / 8) // and add to alpha to make it [r,g,b,a]
+    const shapeColor = hslToRgb(i / 8, 1, 0.5)
+    shapeColor.push(1 / 8 + i / 8) // and add to alpha to make it [r,g,b,a]
     shapes.push(
-      color(shapeColor, translate([(i - 3) * 7.5, 0, 0], cylinder({ r: 3, h: 20 })))
+      colorize(shapeColor, translate([(i - 3) * 7.5, 0, 0], cylinder({ radius: 3, height: 20 })))
     )
   }
   shapes.push(
-    color('red', translate([-4, -10, 0], cuboid(5)))
+    colorize(colorNameToRgb('red'), translate([-4, -10, 0], cuboid({ size: [5, 5, 5] })))
   )
   shapes.push(
-    color('red', 0.5, translate([4, -10, 0], cuboid(5)))
+    colorize([1, 0, 0, 0.5], translate([4, -10, 0], cuboid({ size: [5, 5, 5] })))
   )
   return shapes
 }
