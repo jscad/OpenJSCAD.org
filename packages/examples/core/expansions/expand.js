21f2dda40829cbccab90b650a8e41d5985e55da9
Clark, Simon <sclark@netsuite.com>
2020-08-08 20:44:51 -0400

    Updating expand.js

--- packages/examples/core/expansions/expand.js
+++ packages/examples/core/expansions/expand.js
@@ -5,75 +5,70 @@
 // tags: expand, expansion, operations
 */
 
-const { geom2, geom3, line2, path2 } = require('@jscad/modeling').geometry
-const { cuboid } = require('@jscad/modeling').primitives
+const { cuboid, arc, rectangle } = require('@jscad/modeling').primitives
 const { expand } = require('@jscad/modeling').expansions
-const { color } = require('@jscad/modeling').color
+const { colorize, colorNameToRgb } = require('@jscad/modeling').colors
 
 const main = () => {
   // you can expand 2d paths
-  const points = [
-    [10, 0],
-    [9.510565162951535, 3.090169943749474],
-    [8.090169943749475, 5.877852522924732],
-    [5.877852522924732, 8.090169943749475],
-    [3.0901699437494745, 9.510565162951535],
-    [6.123233995736766e-16, 10]
-  ]
-  const path2Example = color('black',
-    path2.fromPoints({ }, points) // it also works with ccw points ie points.reverse()
+  const path2Example = colorize(colorNameToRgb('black'), arc({ radius: 12, endAngle: Math.PI / 2 }))
+  const expandedPath2Round = colorize(
+    [0.5, 0.5, 1],
+    expand({ delta: 1.7, corners: 'round', segments: 16 }, path2Example)
   )
-  const expandedPath2 = expand({ delta: 2, corners: 'round', segments: 8 }, path2Example)
+  const expandedPath2Edge = colorize(
+    [0.3, 1, 0.3],
+    expand({ delta: 1.5, corners: 'edge', segments: 16 }, path2Example)
+  )
 
   // but also 2d geometry
-  const geom2Example = geom2.fromPoints([[-8, -8], [8, -8], [8, 8], [-8, 8]])
-  const expandedGeom2 = color('red',
-    expand({ delta: 2, corners: 'round', segments: 8 }, geom2Example)
+  const geom2Example = colorize(
+    colorNameToRgb('black'),
+    rectangle({ size: [16, 16] })
   )
-  const contractedGeom2 = color('red',
+
+  const expandedGeom2Chamfer = colorize(
+    [1, 0.5, 0.5],
+    expand({ delta: 1.8, corners: 'chamfer', segments: 16 }, geom2Example)
+  )
+  const expandedGeom2Round = colorize(
+    [0.5, 0.5, 1],
+    expand({ delta: 2, corners: 'round', segments: 16 }, geom2Example)
+  )
+  const expandedGeom2Edge = colorize(
+    [0.3, 1, 0.3],
+    expand({ delta: 2.2, corners: 'edge', segments: 16 }, geom2Example)
+  )
+
+  const contractedGeom2 = colorize(
+    [1, 0.5, 0.5],
     expand({ delta: -2, corners: 'round', segments: 8 }, geom2Example)
   )
 
-  // aaand 3d geometry
-  const geom3Example = color('black',
-    cuboid()
+  // and 3d geometry
+  const geom3Example = colorize(
+    colorNameToRgb('green'),
+    cuboid({ size: [2, 7, 4] })
   )
-  const expandedGeom3 = color('red',
-    expand({ delta: 2, corners: 'round', segments: 8 }, geom2Example)
-  )
-  const contractedGeom3 = color('red',
-    expand({ delta: -2, corners: 'round', segments: 8 }, geom2Example)
+  const expandedGeom3 = colorize(
+    [1, 0, 0, 0.8],
+    expand({ delta: 1, corners: 'round', segments: 32 }, geom3Example)
   )
 
   return [
     path2Example,
-    expandedPath2,
-    //
+    expandedPath2Round,
+    expandedPath2Edge,
+
     geom2Example,
-    expandedGeom2,
+    expandedGeom2Edge,
+    expandedGeom2Chamfer,
+    expandedGeom2Round,
     contractedGeom2,
-    //
+
     geom3Example,
-    expandedGeom3,
-    contractedGeom3
+    expandedGeom3
   ]
-  /*
-  // old example
-  return scale(10, union(
-    expand({ delta: 0.2, corners: 'round', segments: 8 }, difference(cube(2), translate([0.3, -0.3, 0.3], cube(2)))),
-
-    translate([-4, 0, 0], difference(cube(2), translate([0.3, -0.3, 0.3], cube(2)))),
-
-    translate([0, -3, 0], expand({ delta: 0.3, segments: 8 }, cube(2))),
-
-    translate([-4, -3, 0], cube(2)),
-
-    translate([0, 4, 0],
-      expand({ delta: 0.3, segments: 4 }, cylinder({ r: 1, h: 2, fn: 16 }))
-    ),
-
-    translate([-4, 4, 0], cylinder({ r: 1, h: 2 }))
-  )) */
 }
 
 module.exports = { main }
