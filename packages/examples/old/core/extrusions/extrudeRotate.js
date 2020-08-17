a68d757abe244c9f32f26975719a0fc527639b2a
Clark, Simon <sclark@netsuite.com>
2020-08-17 08:36:02 -0400

    parameters/gears.js working

--- packages/examples/old/core/extrusions/extrudeRotate.js
+++ packages/examples/old/core/extrusions/extrudeRotate.js
@@ -4,32 +4,39 @@
 // description: testing extrudeRotate() function
 // tags: extrusion, rotate, extrudeRotate
 */
-
-const { rectangle, circle, polygon } = require('@jscad/modeling').primitives
-const { translate } = require('@jscad/modeling').transforms
-const { extrudeRotate } = require('@jscad/modeling').extrusions
+const jscad = require('@jscad/modeling')
+console.log(jscad)
+const { rectangle, circle, polygon, star } = jscad.primitives
+const { translate } = jscad.transforms
+const { extrudeRotate } = jscad.extrusions
 
 const main = () => {
   return [
-    extrudeRotate(translate([4, 0, 0], circle({ r: 1, segments: 30, center: true }))),
-    extrudeRotate({ segments: 5 },
-      translate([4, 0, 0], circle({ r: 1, segments: 30, center: true }))).translate([0, 10, 0]),
-    extrudeRotate({ segments: 30 },
-      translate([4, 0, 0], circle({ r: 1, segments: 5, center: true }))).translate([0, 20, 0]),
 
-    extrudeRotate({ segments: 4 }, translate([4, 0, 0], rectangle({ size: [1, 1], center: true })))
-      .translate([-10, 0, 0]),
-    extrudeRotate({ segments: 4 }, rectangle({ size: [1, 3], center: true }).translate([4, 0, 0]))
-      .translate([-20, 0, 0]),
-    extrudeRotate({ segments: 3 }, rectangle({ size: [2, 0.5], center: true }).translate([4, 0, 0]))
-      .translate([-20, 10, 0]),
-    extrudeRotate({ segments: 5 }, rectangle({ size: [1, 1], center: true }).translate([4, 0, 0]))
-      .translate([-20, 20, 0]),
+    // smooth torus
+    translate([5, 5, 0], extrudeRotate(
+      { segments: 32 },
+      polygon({points: [[1,0],[3,0],[3,1],[2.2,1],[1.6,2.2],[2,8], [1,8]], closed: true})
+    )),
 
-    extrudeRotate(polygon({ points: [[0, 0], [2, 1], [1, 2], [1, 3], [3, 4], [0, 5]] }))
-      .translate([10, 0, 0]),
-    extrudeRotate({ segments: 4 }, polygon({ points: [[0, 0], [2, 1], [1, 2], [1, 3], [3, 4], [0, 5]] }))
-      .translate([18, 0, 0])
+    // Pentagonal ring
+    translate([5, -5, 0], extrudeRotate(
+      { segments: 5 },
+      translate([4, 0, 0], circle({ radius: 1, segments: 32 }))
+    )),
+
+    // Shooting star
+    translate([-5, -5, 0], extrudeRotate(
+      { segments: 32, startAngle: 0, angle: (Math.PI * 0.75), overflow: 'cap' },
+      translate([4, 0, 0], star({ }))
+    )),
+
+    // Octagonol box
+    translate([-5, 5, 0], extrudeRotate(
+      { segments: 8 },
+      translate([4, 0, 0], rectangle({ size: [1, 3] }))
+    ))
+
   ]
 }
 
