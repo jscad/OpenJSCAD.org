3ed9470906b5eb0c00d9d3c48be94aacd8b44dbc
Z3 Development <z3-dev@gfnews.net>
2020-08-21 10:38:44 +0900

    fix(regl-renderer) changed default meshColor to light blue theme

--- packages/utils/regl-renderer/src/rendering/renderDefaults.js
+++ packages/utils/regl-renderer/src/rendering/renderDefaults.js
@@ -1,6 +1,6 @@
 module.exports = {
   background: [1, 1, 1, 1],
-  meshColor: [1, 0.5, 0.5, 1], // use as default face color for csgs, color for cags
+  meshColor: [0, 0.6, 1, 1], // default face color or line color
   lightColor: [1, 1, 1, 1], // note: for now there is a single preset light, not an entity
   lightDirection: [0.2, 0.2, 1],
   lightPosition: [100, 200, 100],
