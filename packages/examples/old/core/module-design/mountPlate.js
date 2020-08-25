3eee1ece243fb3341df23f7888167c5d1165793f
Clark, Simon <sclark@netsuite.com>
2020-08-24 22:47:15 -0400

    Updating module-design example

--- packages/examples/old/core/module-design/mountPlate.js
+++
@@ -1,7 +0,0 @@
-const { cuboid } = require('@jscad/modeling').primitives
-
-const mountPlate = (length) => {
-  return cuboid({ size: [length, 10, 1] })
-}
-
-module.exports = mountPlate
--- packages/examples/old/core/module-design/subFolder/sphereShape.js
+++
@@ -1,8 +0,0 @@
-const { sphere } = require('@jscad/modeling').primitives
-const color = require('@jscad/modeling').color.color
-
-const sphereShape = (radius) => {
-  return color([1, 0, 0, 1], sphere({ radius }))
-}
-
-module.exports = sphereShape
