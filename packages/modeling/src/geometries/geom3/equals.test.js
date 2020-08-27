3092acd17c49beff0475a6b40552456c7a37f5ba
Z3 Development <z3-dev@gfnews.net>
2020-08-27 11:58:33 +0900

    tests(geometries): additional tests for geom3 and poly3

--- packages/modeling/src/geometries/geom3/equals.test.js
+++ packages/modeling/src/geometries/geom3/equals.test.js
@@ -1,7 +1,17 @@
 const test = require('ava')
+
 const { equals, fromPoints } = require('./index')
 
 test('equals: checks if two geom3 are equal', (t) => {
-  const points = [[[0, 0, 0], [1, 0, 0], [1, 0, 1]]]
-  t.is(equals(fromPoints(points), fromPoints(points)), true)
+  const geomA = fromPoints([[[0, 0, 0], [1, 0, 0], [1, 0, 1]]])
+  const geomB = fromPoints([[[1, 1, 1], [1, 0, 0], [1, 0, 1]]])
+  const geomC = fromPoints([[[0, 0, 0], [1, 0, 0], [1, 0, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]])
+
+  t.is(equals(geomA, geomA), true)
+
+  t.is(equals(geomA, geomB), false)
+  t.is(equals(geomB, geomA), false)
+
+  t.is(equals(geomA, geomC), false)
+  t.is(equals(geomC, geomA), false)
 })
