3e73efc5f53bfd3d5a0343c77ca0956052dc04a4
Z3 Development <z3-dev@gfnews.net>
2020-08-27 11:55:24 +0900

    fix(maths): corrected indexes for mat4 and vec4

--- packages/modeling/src/maths/vec4/index.js
+++ packages/modeling/src/maths/vec4/index.js
@@ -6,6 +6,7 @@
 module.exports = {
   clone: require('./clone'),
   create: require('./create'),
+  dot: require('./dot'),
   fromScalar: require('./fromScalar'),
   fromValues: require('./fromValues'),
   toString: require('./toString'),
