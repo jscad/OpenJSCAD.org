d55e7f0817c96679e96d85e8509685383519f795
Clark, Simon <sclark@netsuite.com>
2020-09-09 22:51:16 -0400

    Tweaks to padArrayToLength.js docs to reflect actual functionality.

--- packages/modeling/src/utils/padArrayToLength.js
+++ packages/modeling/src/utils/padArrayToLength.js
@@ -1,17 +1,17 @@
 /**
- * Fill an array with a default value, until it reaches a specified length.
- * @param {Array} arr - the array to potentially add elements to.
- * @param padding - the element to add to the array to reach the desired length.
+ * Build an array of at minimum a specified length from an existing array and a padding value. IF the array is already larger than the target length, it will not be shortened.
+ * @param {Array} anArray - the source array to copy into the result.
+ * @param {*} padding - the value to add to the new array to reach the desired length.
  * @param {Number} targetLength - The desired length of the return array.
  * @returns {Array} an array of at least 'targetLength" length
  * @alias module:modeling/utils.padArrayToLength
  */
-const padArrayToLength = (arr, padding, targetLength) => {
-  arr = arr.slice()
-  while (arr.length < targetLength) {
-    arr.push(padding)
+const padArrayToLength = (anArray, padding, targetLength) => {
+  anArray = anArray.slice()
+  while (anArray.length < targetLength) {
+    anArray.push(padding)
   }
-  return arr
+  return anArray
 }
 
 module.exports = padArrayToLength
