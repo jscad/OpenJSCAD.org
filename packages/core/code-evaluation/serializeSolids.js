36a1247db8e43ede32f6d1b2663ba686a23b7b37
Z3 Development <z3-dev@gfnews.net>
2020-09-11 09:05:57 +0900

    style(core): removed unused code, and added a note on compactBinary serialization

--- packages/core/code-evaluation/serializeSolids.js
+++ packages/core/code-evaluation/serializeSolids.js
@@ -1,28 +1,14 @@
 const { geometries } = require('@jscad/modeling')
 
-const isGeom2 = geometries.geom2.isA
-const isGeom3 = geometries.geom3.isA
-const isPath2 = geometries.path2.isA
-
-const compactGeom2 = geometries.geom2.toCompactBinary
-const compactGeom3 = geometries.geom3.toCompactBinary
-const compactPath2 = geometries.path2.toCompactBinary
-
 /*
  * Serialize the given solids/objects into web worker message content.
  * @param {Array} solids - list of solids to serialize
  * @return {Array} web worker message contents
  */
 const serializeSolids = (solids) => solids.map((object) => {
-  //  if (isGeom2(object)) {
-  //    return compactGeom2(object)
-  //  } else if (isGeom3(object)) {
-  //    return compactGeom3(object)
-  //  } else if (isPath2(object)) {
-  //    return compactPath2(object)
-  //  } else {
-  //    return JSON.stringify(object)
-  //  }
+  // NOTE: the use of compactBinary formats was removed due to
+  // that lack of support for additional attributes, as well as
+  // imcomplete support for transfering objects via web workers
   return JSON.stringify(object)
 })
 
