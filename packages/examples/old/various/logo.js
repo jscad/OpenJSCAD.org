8c0c329d15ad5ce5c4d74b1e88e0ecc7f03f9c8a
Clark, Simon <sclark@netsuite.com>
2020-09-13 08:34:53 -0400

    reverting logo.js

--- packages/examples/old/various/logo.js
+++ packages/examples/old/various/logo.js
@@ -5,37 +5,28 @@
 // tags       : Logo,Intersection,Sphere,Cube,Big
 // file       : logo_big.jscad
 
-const { cube, sphere } = require('@jscad/modeling').primitives
-const { union, subtract, intersect } = require('@jscad/modeling').booleans
-const { rotate, translate } = require('@jscad/modeling').transforms
-const { colorize } = require('@jscad/modeling').colors
-
-
 function main () {
-  var small = [
-    colorize([1,.6,0.6], translate([0,0,3],rotate([0.3,0.8,0],subtract(
-      cube({size: 3}),
-      sphere({radius: 2})
-    )))),
-    colorize([1,1,0], translate([0,0,3],rotate([0.3,0.8,0],intersect(
-      sphere({radius: 1.3}),
-      cube({size: 2.1})
-    ))))
-  ]
-  return small
+  var small = union(
+      difference(
+          cube({size: 3, center: true}),
+          sphere({r: 2, center: true})
+      ),
+      intersection(
+          sphere({r: 1.3, center: true}),
+          cube({size: 2.1, center: true})
+      )
+  ).translate([0, 0, 1.5]).scale(10);
 
   var large = union(
-    subtract(
-      cube({size: 300, center: true}),
-      sphere({r: 200, center: true})
-    ),
-    intersection(
-      sphere({r: 130, center: true}),
-      cube({size: 210, center: true})
-    )
+      difference(
+          cube({size: 300, center: true}),
+          sphere({r: 200, center: true})
+      ),
+      intersection(
+          sphere({r: 130, center: true}),
+          cube({size: 210, center: true})
+      )
   ).translate([0, 0, 150]);
 
   return small.union(large);
 }
-
-module.exports = {main}
