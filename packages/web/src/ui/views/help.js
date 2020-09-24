15f96a4bdfb61b2920a6b8999ad38e0e738c6eb4
Z3 Development <z3-dev@gfnews.net>
2020-09-24 20:16:44 +0900

    fix(web): V2 web layout tweaks (#687)
    * fix(web) added noscript section in demo.html to warn users to enable javascript
    added handling of beforeunload events to prevent unintended navigation
    
    * fix(web): enabling scrolling of large parameter lists
    
    * fix(web): adjusted meta information for demo
    
    * fix(web): corrected invalid CSS constructs
    
    * fix(web): removed undefined styles from the dynamic html for help
    
    * fix(web): moved CSS property definitions to css directory
    
    * fix(web): added fonts to web content

--- packages/web/src/ui/views/help.js
+++ packages/web/src/ui/views/help.js
@@ -6,11 +6,11 @@ const help = (state, i18n) => {
     <span id='help' style='visibility:${state.activeTool === 'help' ? 'visible' : 'hidden'};' >
       <div>
         <h3>${i18n`Documentation`}:</h3>
-        <p><a class="navlink" href="docs/" target="_blank">${i18n`User Guide`} <img src="imgs/externalLink.png" style="externalLink"></a></p>
+        <p><a class="navlink" href="docs/" target="_blank">${i18n`User Guide`} <img src="imgs/externalLink.png"></a></p>
         <h3>${i18n`Forums`}:</h3>
-        <p><a class="navlink" href="https://jscad.xyz/forum" target="_blank">${i18n`User Group`} <img src="imgs/externalLink.png" style="externalLink"></a></p>
-        <p><a class="navlink" href="https://jscad.xyz/forum/category/1/announcements" rel="publisher" target="_blank">${i18n`Announcements`} <img src="imgs/externalLink.png" style="externalLink"></a></p>
-        <p><a class="navlink" href="https://jscad.xyz/forum" rel="publisher" target="_blank">${i18n`Recent Updates`} <img src="imgs/externalLink.png" style="externalLink"></a></p>
+        <p><a class="navlink" href="https://jscad.xyz/forum" target="_blank">${i18n`User Group`} <img src="imgs/externalLink.png"></a></p>
+        <p><a class="navlink" href="https://jscad.xyz/forum/category/1/announcements" rel="publisher" target="_blank">${i18n`Announcements`} <img src="imgs/externalLink.png"></a></p>
+        <p><a class="navlink" href="https://jscad.xyz/forum" rel="publisher" target="_blank">${i18n`Recent Updates`} <img src="imgs/externalLink.png"></a></p>
       </div>
       <div>
         <h3>${i18n`Examples`}:</h3>
