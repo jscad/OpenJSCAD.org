344fab6b7dbfff13bd4842b84760afe9bf1bc589
Z3 Development <z3-dev@gfnews.net>
2020-10-02 16:42:58 +0900

    fix(modeling): allow geom2.fromPoints() to accept the same closing point as the first point

--- packages/modeling/src/geometries/geom2/fromPoints.js
+++ packages/modeling/src/geometries/geom2/fromPoints.js
@@ -15,16 +15,20 @@ const fromPoints = (points) => {
   if (!Array.isArray(points)) {
     throw new Error('the given points must be an array')
   }
-  if (points.length < 3) {
+  let length = points.length
+  if (length < 3) {
     throw new Error('the given points must define a closed geometry with three or more points')
   }
+  // adjust length if the given points are closed by the same point
+  if (vec2.equals(points[0], points[length - 1])) --length
 
   const sides = []
-  let prevpoint = points[points.length - 1]
-  points.forEach((point) => {
+  let prevpoint = points[length - 1]
+  for (let i = 0; i < length; i++) {
+    const point = points[i]
     sides.push([vec2.fromArray(prevpoint), vec2.fromArray(point)])
     prevpoint = point
-  })
+  }
   return create(sides)
 }
 
