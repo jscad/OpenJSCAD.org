d00c38a360b60014a1ce3ca4bfd404b6ef58575d
Z3 Development <z3-dev@gfnews.net>
2020-10-17 15:44:56 +0900

    fix(core): added indexes for the contents of each sub-directory

---
+++ packages/core/src/code-evaluation/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  deserializeSolids: require('./deserializeSolids'),
+  rebuildGeometry: require('./rebuildGeometry'),
+  rebuildGeometryCli: require('./rebuildGeometryCli'),
+  rebuildGeometryWorker: require('./rebuildGeometryWorker'),
+  serializeSolids: require('./serializeSolids')
+}
---
+++ packages/core/src/io/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  registerAllExtensions: require('./registerExtensions').registerAllExtensions,
+  unRegisterAllExtensions: require('./registerExtensions').unRegisterAllExtensions
+}
