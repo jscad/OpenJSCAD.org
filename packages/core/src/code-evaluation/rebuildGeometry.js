e6ded65b5a4f244e46235bc20ace7a35813e0ff7
Z3 Development <z3-dev@gfnews.net>
2020-10-17 16:10:17 +0900

    fix(core): corrected default options for rebuildGeometry, also added doc strings

--- packages/core/src/code-evaluation/rebuildGeometry.js
+++ packages/core/src/code-evaluation/rebuildGeometry.js
@@ -2,30 +2,45 @@ const loadDesign = require('../code-loading/loadDesign')
 const instanciateDesign = require('./instanciateDesign')
 const applyParameterDefinitions = require('../parameters/applyParameterDefinitions')
 
-/** evaluate script & rebuild solids
- * core, essential function that deals with both extracting the parameters and generation the solids
- * the parsed parameters (definitions & values) & solids are passed to the given 'callback'
+/**
+ * Rebuild JSCAD solids from the given filesAndFolders.
+ * The provided filesAndFolders is expected to consist of a valid JSCAD design.
+ * - either single file or project folder
+ * - single file containing a complete JSCAD design
+ * - project folder following Common JS layout requirements
+ * @param {Object} data - data (and options) required for rebuilding
+ * @param {Array} data.filesAndFolders - array of files / directories, which is the output from walkFileTree
+ * @param {String} [data.mainPath] - path of the file containing the main function (optional)
+ * @param {Boolean} [data.serialize] - true to serialize the solids into JSON
+ * @param {Boolean} [data.vtreeMode] - true to use the experimental Vtree caching (optional)
+ * @param {Object} [data.lookup] - geometry cache lookup (optional)
+ * @param {Object} [data.lookupCounts] - geometry cache lookup counts (optional)
+ * @param {Object} [data.parameterValues] - over-rides of parameter values (optional)
+ * @param {Function} callback - function to process parameters and solids
+ * @return NONE
+ * This function extracts the parameters first, and then generates the solids.
+ * The parsed parameters (definitions and values) are passed back to the given callback function.
+ * The generated solids are also passed back to the given callback function.
  * Everything is together in a single function, because this is usually run in the context of a web worker
  * and transfering data back & forth is both complex (see transferables) and costly (time)
- * @param  {} data should be the raw input data structured like this :
- * { filesAndFolders : the list of files & folders to work on (a flat list of your file system to use)
- *   parameterValues : an object of parameter values (optional),
- *  vtreeMode: {Boolean} Optional use geometry caching or not: if set to true, provide the two following items too
- *  lookup: {Object} Optional geometry cache lookup
- *  lookupCounts: {Object} Optional geometry cache lookup count object
- * }
- * @param  {Function} callback the callback that will be called either with
- * { type: 'params', parameterDefaults, parameterDefinitions} or
- * { type: 'solids', solids, lookup, lookupCounts} lookup & counts are only if using geometry caching (optional & experimantal)
- * example rebuildSolids()
  **/
 const rebuildSolids = (data, callback) => {
-  const defaults = { vtreeMode: true, serialize: true }
-  const { mainPath, vtreeMode, serialize, lookup, lookupCounts } = Object.assign({}, defaults, data)
+  console.log('rebuildSolids',data)
+  const defaults = {
+    mainPath: '',
+    vtreeMode: false,
+    serialize: true,
+    lookup: null,
+    lookupCounts: null,
+    parameterValues: {}
+  }
+  let { mainPath, vtreeMode, serialize, lookup, lookupCounts, parameterValues } = Object.assign({}, defaults, data)
+
   const apiMainPath = '@jscad/modeling'// vtreeMode ? '../code-loading/vtreeApi' : '@jscad/modeling'
+  const filesAndFolders = data.filesAndFolders
 
   // let start = new Date()
-  const designData = loadDesign(mainPath, apiMainPath, data.filesAndFolders, data.parameterValues)
+  const designData = loadDesign(mainPath, apiMainPath, filesAndFolders, parameterValues)
   // send back parameter definitions & values
   // in a worker this would be a postmessage, this is sent back early so that uis can update
   // the parameters editor before the solids are displayed (which takes longer)
@@ -37,7 +52,6 @@ const rebuildSolids = (data, callback) => {
   // console.warn(`loadDesignData`, new Date() - start)
   // make sure parameters are correct by applying parameter definitions
   // this might be redundant with ui-side logic, but it makes sure this core piece works regardless of ui
-  let parameterValues = data.parameterValues
   parameterValues = applyParameterDefinitions(parameterValues, designData.parameterDefinitions)
   parameterValues = Object.assign({}, designData.parameterValues, parameterValues)
   // start = new Date()
