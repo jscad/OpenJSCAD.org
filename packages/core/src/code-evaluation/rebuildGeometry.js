42743b962ef78c70c1a7e1842038d4adbd4b3cf6
Z3 Development <z3-dev@gfnews.net>
2020-10-23 14:04:49 +0900

    docs(core): improved docs for rebuildGeometry

--- packages/core/src/code-evaluation/rebuildGeometry.js
+++ packages/core/src/code-evaluation/rebuildGeometry.js
@@ -5,11 +5,11 @@ const applyParameterDefinitions = require('../parameters/applyParameterDefinitio
 /**
  * Rebuild JSCAD solids from the given filesAndFolders.
  * The provided filesAndFolders is expected to consist of a valid JSCAD design.
- * - either single file or project folder
- * - single file containing a complete JSCAD design
- * - project folder following Common JS layout requirements
+ * An array consisting of:
+ * - single file or project folder from the results of walkFileTree()
+ * - fake single file entry containing { name, ext, source, fullPath }
  * @param {Object} data - data (and options) required for rebuilding
- * @param {Array} data.filesAndFolders - array of files / directories, which is the output from walkFileTree
+ * @param {Array} data.filesAndFolders - array of files / directories
  * @param {String} [data.mainPath] - path of the file containing the main function (optional)
  * @param {Boolean} [data.serialize] - true to serialize the solids into JSON
  * @param {Boolean} [data.vtreeMode] - true to use the experimental Vtree caching (optional)
@@ -22,7 +22,7 @@ const applyParameterDefinitions = require('../parameters/applyParameterDefinitio
  * The parsed parameters (definitions and values) are passed back to the given callback function.
  * The generated solids are also passed back to the given callback function.
  * Everything is together in a single function, because this is usually run in the context of a web worker
- * and transfering data back & forth is both complex (see transferables) and costly (time)
+ * And transfering data back & forth is both complex (see transferables) and costly (time)
  **/
 const rebuildSolids = (data, callback) => {
   console.log('rebuildSolids',data)
