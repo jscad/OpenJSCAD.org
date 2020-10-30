3952b82030147f1bf710b44349ba3c066ab8073c
Michael Williams <michael.williams@enspiral.com>
2020-10-30 16:46:51 +1300

    generate typescript declarations from jsdoc
    an experiment!

--- packages/modeling/src/geometries/geom2/create.js
+++ packages/modeling/src/geometries/geom2/create.js
@@ -1,15 +1,22 @@
+/**
+ * @typedef { import("../../maths/mat4/type") } mat4
+ * @typedef { import("../../maths/vec2/type") } vec2
+ * @typedef { import("./type") } geom2
+ * @typedef {[vec2, vec2]} side
+ * @typedef {Array<side>} sides
+ */
+
 const mat4 = require('../../maths/mat4')
 
 /**
  * Represents a 2D geometry consisting of a list of sides.
- * @typedef {Object} geom2
- * @property {Array} sides - list of sides, each side containing two points
+ * @property {sides} sides - list of sides, each side containing two points
  * @property {mat4} transforms - transforms to apply to the sides, see transform()
  */
 
 /**
  * Create a new 2D geometry composed of unordered sides (two connected points).
- * @param {Array} [sides] - list of sides where each side is an array of two points
+ * @param {sides} [sides] - list of sides where each side is an array of two points
  * @returns {geom2} a new empty geometry
  * @alias module:modeling/geometries/geom2.create
  */
