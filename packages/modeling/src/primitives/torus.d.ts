aea420fd1b40d229ea734a1016a2e251c53f0259
Michael Williams <michael.williams@enspiral.com>
2020-11-11 12:46:46 +1300

    errata

--- packages/modeling/src/primitives/torus.d.ts
+++ packages/modeling/src/primitives/torus.d.ts
@@ -4,13 +4,13 @@ import Geom3 from '../geometries/geom3/type'
 export default torus
 
 export interface TorusOptions {
-  innerRadius: number
-  outerRadius: number
-  innerSegments: number
-  outerSegments: number
-  innerRotation: number
-  outerRotation: number
-  startAngle: number
+  innerRadius?: number
+  outerRadius?: number
+  innerSegments?: number
+  outerSegments?: number
+  innerRotation?: number
+  outerRotation?: number
+  startAngle?: number
 }
 
 declare function torus(options?: TorusOptions): Geom3
