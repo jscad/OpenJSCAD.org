d658a3700c3d6919a99b0cd4517447c11f749c62
Michael Williams <michael.williams@enspiral.com>
2020-11-13 11:47:50 +1300

    s/offset/factor
    ref: https://github.com/jscad/OpenJSCAD.org/pull/726#discussion_r521769079

--- packages/modeling/src/operations/transforms/scale.d.ts
+++ packages/modeling/src/operations/transforms/scale.d.ts
@@ -4,11 +4,11 @@ import RecursiveArray from '../../utils/recursiveArray'
 export function scale(factors: [number, number, number], geometry: Geometry): Geometry
 export function scale(factors: [number, number, number], ...geometries: RecursiveArray<Geometry>): Array<Geometry>
 
-export function scaleX(offset: number, geometry: Geometry): Geometry
-export function scaleX(offset: number, ...geometries: RecursiveArray<Geometry>): Array<Geometry>
+export function scaleX(factor: number, geometry: Geometry): Geometry
+export function scaleX(factor: number, ...geometries: RecursiveArray<Geometry>): Array<Geometry>
 
-export function scaleY(offset: number, geometry: Geometry): Geometry
-export function scaleY(offset: number, ...geometries: RecursiveArray<Geometry>): Array<Geometry>
+export function scaleY(factor: number, geometry: Geometry): Geometry
+export function scaleY(factor: number, ...geometries: RecursiveArray<Geometry>): Array<Geometry>
 
-export function scaleZ(offset: number, geometry: Geometry): Geometry
-export function scaleZ(offset: number, ...geometries: RecursiveArray<Geometry>): Array<Geometry>
+export function scaleZ(factor: number, geometry: Geometry): Geometry
+export function scaleZ(factor: number, ...geometries: RecursiveArray<Geometry>): Array<Geometry>
