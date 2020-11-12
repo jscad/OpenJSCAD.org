5b6fa0196753d568362f87f950077aa0cafbb978
Michael Williams <michael.williams@enspiral.com>
2020-11-13 11:55:55 +1300

    remove temporary type
    ref: https://github.com/jscad/OpenJSCAD.org/pull/726#discussion_r522476939

--- packages/modeling/src/colors/colorize.d.ts
+++ packages/modeling/src/colors/colorize.d.ts
@@ -3,9 +3,7 @@ import RecursiveArray from '../utils/recursiveArray'
 
 import { RGB, RGBA } from './types'
 
-type Color = RGB | RGBA
-
 export default colorize
 
-declare function colorize<T extends Geometry>(color: Color, object: T): T
-declare function colorize(color: Color, ...objects: RecursiveArray<Geometry>): Array<Geometry>
+declare function colorize<T extends Geometry>(color: RGB | RGBA, object: T): T
+declare function colorize(color: RGB | RGBA, ...objects: RecursiveArray<Geometry>): Array<Geometry>
