01e8c6e7ec292df8fe1807db9f8547023005d14b
Mikey <michael.williams@enspiral.com>
2020-11-20 15:01:01 +1300

    docs(modeling) : document changes (#732)
    * fix cylinderElliptic.height param
    
    * fix cylinder.height param
    
    * p1 -> point1 and p2 -> point2
    
    * fix operations/transforms/align grouped type

--- packages/modeling/src/primitives/cylinder.js
+++ packages/modeling/src/primitives/cylinder.js
@@ -7,7 +7,7 @@ const { isGT } = require('./commonChecks')
  * @see [cylinderElliptic]{@link module:modeling/primitives.cylinderElliptic} for more options
  * @param {Object} [options] - options for construction
  * @param {Array} [options.center=[0,0,0]] - center of cylinder
- * @param {Array} [options.height=2] - height of cylinder
+ * @param {Number} [options.height=2] - height of cylinder
  * @param {Number} [options.radius=1] - radius of cylinder (at both start and end)
  * @param {Number} [options.segments=32] - number of segments to create per full rotation
  * @returns {geom3} new geometry
