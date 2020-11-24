c09ba70fa4aebbee57566a38aed3af03744ef397
Davor Hrg <hrgdavor@gmail.com>
2020-11-24 10:47:20 +0100

    V2 feature livereload (#729)
    * livereload initial attempt
    
    * improve timing for starting websocket
    
    * standardx lint
    
    * few tweaks for review
    
    * changes based on feedback
    
    * review

--- packages/web/src/sideEffects/localFs/index.js
+++ packages/web/src/sideEffects/localFs/index.js
@@ -3,8 +3,39 @@ const { callbackToObservable } = require('@jscad/core').observableUtils
 
 const { changedFiles, flattenFiles } = require('./utils')
 
+const localFsOptions = require('../../../data/localFsOptions.json')
+
 const makeLocalFsSideEffect = async (params) => {
   const commandResponses = callbackToObservable()
+
+  let webSocket = null
+  let lastCheck = 0
+
+  const sendWs = (m) => webSocket.send(JSON.stringify(m))
+
+  const startWebSocket = () => {
+    webSocket = new WebSocket(localFsOptions.livereloadUrl)
+
+    webSocket.onopen = function (event) {
+      console.log('websocket open', event)
+      sendWs({ command: 'hello', protocols: ['http://livereload.com/protocols/official-6', 'http://livereload.com/protocols/official-7'], ver: '2.0.8', ext: 'Chrome', extver: '2.1.0' })
+    }
+
+    webSocket.onmessage = function (event) {
+      const data = JSON.parse(event.data)
+      if (data.command === 'hello') sendWs({ command: 'info', plugins: { less: { disable: false, version: '1.0' } }, url: document.location })
+      if (data.command === 'reload') lastCheck = 0
+    }
+
+    webSocket.onerror = function (event) {
+      console.log('websocket error', event)
+    }
+
+    webSocket.onclose = function (event) {
+      console.log('websocket closed', event)
+      webSocket = null
+    }
+  }
 
   const sink = (commands$) => {
     let currentFileTree
@@ -14,7 +45,6 @@ const makeLocalFsSideEffect = async (params) => {
 
     // every time a new command is recieved (observable)
     commands$.forEach((command) => {
-      // console.log('command', command)
       const { type, id, data, options, path } = command
 
       // command handlers/ response
@@ -27,8 +57,11 @@ const makeLocalFsSideEffect = async (params) => {
         currentFileTree = undefined
         rawData = undefined
         if (watcher) {
-          clearTimeout(watcher)
+          clearInterval(watcher)
           watcher = 0
+          if (webSocket) {
+            webSocket.close()
+          }
         }
         if (!(data.length && (data[0] instanceof File))) rawData = data // only watch live FileSystem data
 
@@ -44,8 +77,14 @@ const makeLocalFsSideEffect = async (params) => {
 
         const { enabled } = options
         if (enabled) {
-          const walkAndCheck = () => {
+          if (!webSocket) startWebSocket()
+
+          watcher = setInterval(() => {
             const startMs = Date.now()
+            
+            if (!rawData || (webSocket && lastCheck) || (startMs - lastCheck) < watcherDelay) return
+            lastCheck = startMs
+
             walkFileTree(rawData)
               .catch((error) => {
                 console.error('failed to read files', error)
@@ -61,13 +100,11 @@ const makeLocalFsSideEffect = async (params) => {
 
                 const endMs = Date.now()
                 watcherDelay = Math.max((endMs - startMs) * 2, 1000)
-                watcher = setTimeout(walkAndCheck, watcherDelay)
               })
-          }
-          watcher = setTimeout(walkAndCheck, watcherDelay)
+          }, 50)
         } else {
           if (watcher) {
-            clearTimeout(watcher)
+            clearInterval(watcher)
             watcher = 0
           }
         }
