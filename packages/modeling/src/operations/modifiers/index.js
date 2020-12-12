fe59b112b6d33305f5d691a441546d48682b5dc8
Z3 Development <z3-dev@gfnews.net>
2020-12-12 14:20:25 +0900

    feat(modeling) : added snap to API under new class of operations; modifiers

---
+++ packages/modeling/src/operations/modifiers/index.js
@@ -0,0 +1,10 @@
+/**
+ * All shapes (primitives or the results of operations) can be modified to correct issues, etc.
+ * In all cases, these functions returns the results, and never changes the original geometry.
+ * @module modeling/modifiers
+ * @example
+ * const { snap } = require('@jscad/modeling').modifiers
+ */
+module.exports = {
+  snap: require('./snap')
+}
---
+++ packages/modeling/src/operations/modifiers/snapPolygons.js
@@ -0,0 +1,24 @@
+const vec3 = require('../../maths/vec3')
+
+const poly3 = require('../../geometries/poly3')
+
+const isValidPoly3 = (epsilon, polygon) => {
+  if (poly3.isA(polygon)) {
+    const area = poly3.measureArea(polygon)
+    return (Number.isFinite(area) && area > epsilon)
+  }
+  return false
+}
+
+const snapPolygons = (epsilon, polygons) => {
+  let newpolygons = polygons.map((polygon) => {
+    const newvertices = polygon.vertices.map((vertice) => vec3.snap(vec3.create(), epsilon, vertice))
+    return poly3.create(newvertices)
+  })
+  // snap can produce polygons with zero (0) area, remove those
+  const epsilonArea = (epsilon * epsilon * Math.sqrt(3) / 4)
+  newpolygons = newpolygons.filter((polygon) => isValidPoly3(epsilonArea, polygon))
+  return newpolygons
+}
+
+module.exports = snapPolygons
