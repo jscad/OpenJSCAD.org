203b9235ce3afb998eea79d19d1f3006394cb1f8
z3dev <z3-dev@gfnews.net>
2021-04-14 14:06:30 +0900

    refactor(regl-renderer): simplified computeBounds

--- packages/utils/regl-renderer/src/bound-utils/computeBounds.js
+++ packages/utils/regl-renderer/src/bound-utils/computeBounds.js
@@ -1,5 +1,7 @@
 const vec3 = require('gl-vec3')
 
+const { flatten } = require('@jscad/array-utils')
+
 const boundingBox = require('./boundingBox')
 
 /*
@@ -15,47 +17,29 @@ const boundingBox = require('./boundingBox')
  *   size: [6,20,4]
  * }
  */
-const computeBounds = (geometries) => {
-  let dia
-  let center
+const computeBounds = (...geometries) => {
+  geometries = flatten(geometries)
+
   let bbox // min and max
-  let size
-  let min
-  let max
-  if (Array.isArray(geometries)) {
-    geometries.forEach((geometry) => {
-      let gbbox = boundingBox(geometry.positions)
-      gbbox = gbbox.map((bounds) => vec3.transformMat4(bounds, bounds, geometry.transforms))
-      if (bbox) {
-        vec3.min(bbox[0], bbox[0], gbbox[0])
-        vec3.max(bbox[1], bbox[1], gbbox[1])
-      } else {
-        bbox = gbbox
-      }
-    })
+  geometries.forEach((geometry) => {
+    let gbbox = boundingBox(geometry.positions)
+    gbbox = gbbox.map((bounds) => vec3.transformMat4(bounds, bounds, geometry.transforms))
+    if (bbox) {
+      vec3.min(bbox[0], bbox[0], gbbox[0])
+      vec3.max(bbox[1], bbox[1], gbbox[1])
+    } else {
+      bbox = gbbox
+    }
+  })
 
-    min = vec3.min(vec3.create(), bbox[1], bbox[0])
-    max = vec3.max(vec3.create(), bbox[1], bbox[0])
+  const min = vec3.min(vec3.create(), bbox[1], bbox[0])
+  const max = vec3.max(vec3.create(), bbox[1], bbox[0])
 
-    size = vec3.subtract(vec3.create(), max, min)
-    center = vec3.scale(vec3.create(), size, 0.5)
-    center = vec3.add(center, min, center)
-    // aproximate diamter
-    dia = vec3.distance(center, max)
-  } else {
-    const geometry = geometries
-    bbox = boundingBox(geometry.positions)
-    bbox = bbox.map((bounds) => vec3.transformMat4(bounds, bounds, geometry.transforms))
-
-    min = vec3.min(vec3.create(), bbox[1], bbox[0])
-    max = vec3.max(vec3.create(), bbox[1], bbox[0])
-
-    size = vec3.subtract(vec3.create(), max, min)
-    center = vec3.scale(vec3.create(), size, 0.5)
-    center = vec3.add(center, min, center)
-    // aproximate diamter
-    dia = vec3.distance(center, max)
-  }
+  const size = vec3.subtract(vec3.create(), max, min)
+  let center = vec3.scale(vec3.create(), size, 0.5)
+  center = vec3.add(center, min, center)
+  // aproximate diamter
+  const dia = vec3.distance(center, max)
 
   const bounds = {
     dia,
