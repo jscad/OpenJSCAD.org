15eea8ea643011ad67f02b525e3cd364490dc17d
z3dev <z3-dev@gfnews.net>
2021-05-21 10:24:00 +0900

    docs(modeling): corrected TS declaration for aboutEqualNormals

--- packages/modeling/src/maths/utils/aboutEqualNormals.d.ts
+++ packages/modeling/src/maths/utils/aboutEqualNormals.d.ts
@@ -2,4 +2,4 @@ import Vec3 from '../vec3/type'
 
 export default aboutEqualNormals
 
-declare function aboutEqualNormals(a: Vec3, b: Vec3): number
+declare function aboutEqualNormals(a: Vec3, b: Vec3): boolean
