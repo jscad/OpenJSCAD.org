f42145b10c352233336696b46fc53845aa0bf5d5
minodisk <daisuke.mino@gmail.com>
2021-05-27 17:36:15 +0900

    Delete TypeScript test files

--- packages/modeling/src/operations/extrusions/slice/create.test.ts
+++
@@ -1,9 +0,0 @@
-import test from 'ava'
-
-import { create } from './index'
-
-test('slice: create() should return a slice with initial values', (t) => {
-  const obs = create()
-  const exp = { edges: [] }
-  t.deepEqual(obs, exp)
-})
