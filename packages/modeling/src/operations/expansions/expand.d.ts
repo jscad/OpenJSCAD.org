e8162b5ba303d55caba789ed0ac288cbfa9d5bdf
minodisk <daisuke.mino@gmail.com>
2021-05-31 22:59:18 +0900

    Fix the argument and return types when the argument of expand is a RecursiveArray

--- packages/modeling/src/operations/expansions/expand.d.ts
+++ packages/modeling/src/operations/expansions/expand.d.ts
@@ -10,7 +10,9 @@ export interface ExpandOptions {
   segments?: number
 }
 
+type Geom = Path2 | Geom2 | Geom3
+
 declare function expand(options: ExpandOptions, geometry: Path2 | Geom2): Geom2
 declare function expand(options: ExpandOptions, geometry: Geom3): Geom3
-declare function expand(options?: ExpandOptions, ...geometries: RecursiveArray<Path2 | Geom2>): Geom2
-declare function expand(options?: ExpandOptions, ...geometries: RecursiveArray<Path2 | Geom2 | Geom3>): Geom3
+declare function expand<T extends Geom>(options?: ExpandOptions, ...geometries: RecursiveArray<T>): Array<T>
+declare function expand(options?: ExpandOptions, ...geometries: RecursiveArray<Geom>): Array<Geom>
