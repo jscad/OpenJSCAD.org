ef0fbc8221fe73c5ffefcac8bc85b271cebb7a99
minodisk <daisuke.mino@gmail.com>
2021-05-31 22:39:32 +0900

    Make the return type a flat array

--- packages/modeling/src/colors/colorize.d.ts
+++ packages/modeling/src/colors/colorize.d.ts
@@ -8,5 +8,5 @@ export default colorize
 declare function colorize<T extends Geometry>(color: RGB | RGBA, object: T): T
 declare function colorize<T>(color: RGB | RGBA, object: T): T & Colored
 
-declare function colorize<T extends RecursiveArray<Geometry>>(color: RGB | RGBA, ...objects: T): T
-declare function colorize(color: RGB | RGBA, ...objects: RecursiveArray<any>): RecursiveArray<any & Colored>
+declare function colorize<T>(color: RGB | RGBA, ...objects: RecursiveArray<T>): Array<T & Colored>
+declare function colorize(color: RGB | RGBA, ...objects: RecursiveArray<any>): Array<any & Colored>
