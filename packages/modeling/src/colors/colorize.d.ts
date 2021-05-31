ddb8c62b186b644525ff71d6e9e101d8fc0a8789
minodisk <daisuke.mino@gmail.com>
2021-05-31 22:48:32 +0900

    Added a pattern to colorize that takes a recursive array composed of types that inherit from Geometry as an argument

--- packages/modeling/src/colors/colorize.d.ts
+++ packages/modeling/src/colors/colorize.d.ts
@@ -8,5 +8,6 @@ export default colorize
 declare function colorize<T extends Geometry>(color: RGB | RGBA, object: T): T
 declare function colorize<T>(color: RGB | RGBA, object: T): T & Colored
 
+declare function colorize<T extends Geometry>(color: RGB | RGBA, ...objects: RecursiveArray<T>): Array<T>
 declare function colorize<T>(color: RGB | RGBA, ...objects: RecursiveArray<T>): Array<T & Colored>
 declare function colorize(color: RGB | RGBA, ...objects: RecursiveArray<any>): Array<any & Colored>
