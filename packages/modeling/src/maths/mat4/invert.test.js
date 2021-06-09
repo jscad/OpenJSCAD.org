3eedab3ba1f8a8f3e909eeeb74dca230af8ac435
Davor Hrg <hrgdavor@gmail.com>
2021-06-09 04:43:32 +0200

    feat(modeling): added mat4.invert (#860)

---
+++ packages/modeling/src/maths/mat4/invert.test.js
@@ -0,0 +1,29 @@
+const test = require('ava')
+const { create, invert, fromTranslation, fromXRotation } = require('./index')
+
+const { compareVectors } = require('../../../test/helpers/index')
+const vec3 = require('../vec3/index')
+
+test('mat4: invert() translate ', (t) => {
+  const matrix = fromTranslation(create(), [10, 10, 0])
+  const matrixInv = invert(create(), matrix)
+
+  const vec1 = [0, 0, 0]
+  const vec2 = vec3.transform([0, 0, 0], vec1, matrix)
+  t.true(compareVectors(vec2, [10, 10, 0]))
+
+  const vec2back = vec3.transform([0, 0, 0], vec2, matrixInv)
+  t.true(compareVectors(vec2back, vec1))
+})
+
+test('mat4: invert() rotate ', (t) => {
+  const matrix = fromXRotation(create(), Math.PI / 2)
+  const matrixInv = invert(create(), matrix)
+
+  const vec1 = [10, 10, 10]
+  const vec2 = vec3.transform([0, 0, 0], vec1, matrix)
+  t.true(compareVectors(vec2, [10, -10, 10]))
+
+  const vec2back = vec3.transform([0, 0, 0], vec2, matrixInv)
+  t.true(compareVectors(vec2back, vec1))
+})
