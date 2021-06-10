24c898d8860db023276621f705da3d9aaef0895d
Davor Hrg <hrgdavor@gmail.com>
2021-06-10 08:20:17 +0200

    Update serializeSolids.js

--- packages/core/src/code-evaluation/serializeSolids.js
+++ packages/core/src/code-evaluation/serializeSolids.js
@@ -9,6 +9,10 @@ const serializeSolids = (solids) => {
   // NOTE: the use of compactBinary formats was removed due to
   // that lack of support for additional attributes, as well as
   // imcomplete support for transfering objects via web workers
+
+  // NOTE: JSON.stringify was used at some point, but was removed because it was no longer needed
+  // for postMessage JavaScript engines now use an optimized structured clone alg. 
+  // which should be at least as fast as JSON.stringify
   solids = solids.map((object) => {
     // apply the transforms before serializing
     if (geom2.isA(object)) geom2.toSides(object)
