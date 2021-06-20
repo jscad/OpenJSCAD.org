1e4111dd64052c8fb2c687b45739ddcdb06eb7bc
Davor Hrg <hrgdavor@gmail.com>
2021-06-20 23:20:49 +0200

    Update transform.js

--- packages/modeling/src/geometries/geom3/transform.js
+++ packages/modeling/src/geometries/geom3/transform.js
@@ -3,7 +3,6 @@ const mat4 = require('../../maths/mat4')
 /**
  * Transform the given geometry using the given matrix.
  * This is a lazy transform of the polygons, as this function only adjusts the transforms.
- * See applyTransforms() for the actual application of the transforms to the polygons.
  * @param {mat4} matrix - the matrix to transform with
  * @param {geom3} geometry - the geometry to transform
  * @returns {geom3} a new geometry
