21a87a35cb250bc8b0ed7cff19825b6b1cb1aa1e
z3dev <z3-dev@gfnews.net>
2021-09-07 16:10:35 +0900

    docs(all): correcting documentation

--- packages/modeling/src/curves/bezier/valueAt.js
+++ packages/modeling/src/curves/bezier/valueAt.js
@@ -1,16 +1,16 @@
 /**
- * Calculates the position at a specific point along a bezier easing curve.
+ * Calculates the value at a specific position along a bezier easing curve.
  * For multidimensional curves, the tangent is the slope of each dimension at that point.
- * See the extrudeAlongPath.js example to see this in use.
+ * See the example called extrudeAlongPath.js to see this in use.
  * Math and explanation comes from {@link https://www.freecodecamp.org/news/nerding-out-with-bezier-curves-6e3c0bc48e2f/}
  *
  * @example
  * const b = bezier.create([0,0,0], [0,5,10], [10,0,-5], [10,10,10]]) // a cubic 3 dimensional easing curve that can generate position arrays for modelling
  * let position = bezier.valueAt(t,b) // where 0 < t < 1
  *
- * @param {number} t The position that you want the bezier's tangent value at.
- * @param {Object} bezier A bezier curve created with bezier.create().
- * @returns {array | number} the tangent at the position t.
+ * @param {number} t : the position of which to calculate the value; 0 < t < 1
+ * @param {Object} bezier : a bezier curve created with bezier.create().
+ * @returns {array | number} the value at the requested position.
  * @alias module:modeling/curves/bezier.valueAt
  */
 const valueAt = (t, bezier) => {
