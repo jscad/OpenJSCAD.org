4ea11966af4b3ecbb5c1a3d53b7ac90c4cd349b1
Z3 Development <z3-dev@gfnews.net>
2021-09-07 17:46:01 +0900

    fix(web): added open issues and discord community to help
    * docs(modeling): correct definitions of modifiers
    * docs(all): correct glaring errors in README files
    * docs(all): correcting documentation

--- packages/modeling/src/curves/bezier/tangentAt.js
+++ packages/modeling/src/curves/bezier/tangentAt.js
@@ -1,15 +1,15 @@
 /**
- * Calculates the tangent at a specific point along a bezier easing curve.
+ * Calculates the tangent at a specific position along a bezier easing curve.
  * For multidimensional curves, the tangent is the slope of each dimension at that point.
- * See the extrudeAlongPath.js example to see this in use.
+ * See the example called extrudeAlongPath.js
  *
  * @example
  * const b = bezier.create([0,0,0], [0,5,10], [10,0,-5], [10,10,10]]) // a cubic 3 dimensional easing curve that can generate position arrays for modelling
- * let tangent = bezier.tangentAt(t,b) // where 0 < t < 1
+ * let tangent = bezier.tangentAt(t, b)
  *
- * @param {number} t The position that you want the bezier's tangent value at.
- * @param {Object} bezier An array with at least 2 elements of either all numbers, or all arrays of numbers that are the same size.
- * @returns {array | number} the tangent at the position.
+ * @param {number} t : the position of which to calculate the bezier's tangent value; 0 < t < 1
+ * @param {Object} bezier : an array with at least 2 elements of either all numbers, or all arrays of numbers that are the same size.
+ * @returns {array | number} the tangent at the requested position.
  * @alias module:modeling/curves/bezier.tangentAt
  */
 const tangentAt = (t, bezier) => {
