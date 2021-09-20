1f37c5cd88633566cc2afb12a6cb0e22537c4bb0
Davor Hrg <hrgdavor@gmail.com>
2021-09-20 09:52:06 +0200

    revert special handling err in status

--- packages/web/src/ui/views/status.js
+++ packages/web/src/ui/views/status.js
@@ -4,12 +4,8 @@ const html = require('nanohtml')
 const status = (state, paramsCallbacktoStream) => {
   const status = state.status
   const errorMessage = status.error !== undefined && status.error.message ? `${status.error.message}` : ''
-  let errorLine = status.error !== undefined && status.error.lineno ? `Line: ${status.error.lineno}` : ''
+  const errorLine = status.error !== undefined && status.error.lineno ? `Line: ${status.error.lineno}` : ''
   const errorStack = status.error !== undefined && status.error.stack ? `Stack: ${status.error.stack}` : ''
-
-  // for now could not find a way to apss right line number from custom error that knows
-  // the correct line number the error originates. So added support for line number inside the message
-  if(errorMessage.toLocaleLowerCase().indexOf('line:') !== -1) errorLine = ''
 
   const statusMessage = status.error !== undefined ? html`<span>
     <div>
