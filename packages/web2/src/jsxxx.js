3af8c4d4a78bb0d53d726ba668d4d381cd372608
Davor Hrg <hrgdavor@gmail.com>
2021-10-31 09:04:52 +0100

    some more bits

--- packages/web2/src/jsxxx.js
+++ packages/web2/src/jsxxx.js
@@ -1,8 +1,8 @@
-let ct, ce, TRANS={}, dirty=new Set(), anim = requestAnimationFrame, hasDirty = false, translationUpdaters = []
+let ct; let ce; const TRANS = {}; const dirty = new Set(); let anim = requestAnimationFrame; let hasDirty = false; const translationUpdaters = []
 
 if (typeof document !== 'undefined') {
-  ct = (...args)=>document.createTextNode(...args)
-  ce = (...args)=>document.createElement(...args)
+  ct = (...args) => document.createTextNode(...args)
+  ce = (...args) => document.createElement(...args)
 }
 
 export function setTranslations (trans) {
@@ -22,99 +22,107 @@ export function h (tag, attr, ...children) {
   return { tag, attr, children }
 }
 
-function updateText(node, func){
-  var ret = function(){
-    var newValue = func();
+function updateText (node, func) {
+  const ret = function () {
+    let newValue = func()
     // TODO join text node updating and value handling
-    if(newValue === null || newValue === void 0) newValue = '';		
-    if(typeof(newValue) != 'string') newValue = ''+newValue;
-    if(node.textContent != newValue) node.textContent = newValue;
+    if (newValue === null || newValue === undefined) newValue = ''
+    if (typeof (newValue) !== 'string') newValue = '' + newValue
+    if (node.textContent !== newValue) node.textContent = newValue
   }
-  ret.node = node;
-return ret;
+  ret.node = node
+  return ret
 }
 
-export function runDirty(){
-    dirty.forEach(runUpdaters)
-    dirty.clear()
-    hasDirty = false
+export function runDirty () {
+  dirty.forEach(runUpdaters)
+  dirty.clear()
+  hasDirty = false
 }
 
-export function addDirty(updaters) {
-    dirty.add(updaters)
-    if(!hasDirty){
-        // once first dirty is marked, request animation frame, but only once
-        hasDirty = true
-        anim(runDirty)
-    } 
+export function addDirty (updaters) {
+  dirty.add(updaters)
+  if (!hasDirty) {
+    // once first dirty is marked, request animation frame, but only once
+    hasDirty = true
+    anim(runDirty)
+  }
 }
 
-export function runUpdaters(updaters) {
-    let len = updaters.length
-    for(let i=0;i<len;i++){
-        try {
-            updaters[i]()
-        } catch (error) {
-            console.error(error)
-        }
+export function runUpdaters (updaters) {
+  const len = updaters.length
+  for (let i = 0; i < len; i++) {
+    try {
+      updaters[i]()
+    } catch (error) {
+      console.error(error)
     }
+  }
 }
 
-export function makeUpdater(){
-    let updaters = []
-    let $$ = (f)=>{
-        if(typeof f === 'function'){
-            return (...params)=>{
-                addDirty(updaters)
-                return f(...params)
-            }
-        }else{
-            addDirty(updaters)
-            return f
-        }
+export function makeUpdater () {
+  const updaters = []
+  const $$ = (f) => {
+    if (typeof f === 'function') {
+      const out = (...params) => {
+        addDirty(updaters)
+        return f(...params)
+      }
+      out.addUpdater = (updater) => updaters.push(updater)
+      return out
+    } else {
+      addDirty(updaters)
+      return f
     }
-    $$.push = (updater)=>updaters.push(updater)
-    $$.dirty = ()=>addDirty(updaters)
-    return $$
+  }
+  $$.push = (updater) => updaters.push(updater)
+  $$.dirty = () => addDirty(updaters)
+  return $$
 }
 
-function pushUpdaters(updaters, def, updater){
-    // allow updater function to be refreshad from somewhere else, liver translations could use this
-    if(def.addUpdater) def.addUpdater(updater)
-    else updaters.push(updater)
+function pushUpdaters (updaters, func, updater) {
+  // allow updater function to be refreshad from somewhere else, liver translations could use this
+  if (func.addUpdater) {
+    func.addUpdater(updater)
+  } else {
+    updaters.push(updater)
+  }
 }
 
 /** insert HMTL based on tag description */
-export function insertHtml (parent, before, def, comp={}, updaters=[]) {
+export function insertHtml (parent, before, def, comp = {}, updaters = []) {
   let out
   if (typeof def === 'string') {
     out = ct(def)
     parent.insertBefore(out, before)
-} else if (def instanceof Function) {
+  } else if (def instanceof Function) {
     out = ct(def())
     parent.insertBefore(out, before)
-    pushUpdaters(updaters, def, updateText(out,def))
-} else if (def instanceof Array) {
+    pushUpdaters(updaters, def, updateText(out, def))
+  } else if (def instanceof Array) {
     out = def.map(c => insertHtml(parent, before, c, comp, updaters))
   } else {
     out = ce(def.tag)
+
     if (def.attr) {
       for (const a in def.attr) {
-        let value = def.attr[a]
-        if(a[0] === 'o' && a[1] === 'n' && value instanceof Function){
-            out.addEventListener(a.substring(2), value)
-        }else{
-            out.setAttribute(a, value)
-            if(a === 'p'){
-                let idx = value.indexOf('.')
-                if(idx === -1){
-                    comp[value] = out
-                }else{
-                    let gcode = value.substring(0,idx)
-                    if(!comp[gcode]) comp[gcode] = {}
-                    comp[gcode][value.substring(idx+1)] = out
-                }
+        const value = def.attr[a]
+        if (a[0] === 'o' && a[1] === 'n' && value instanceof Function) {
+          out.addEventListener(a.substring(2), value)
+        } else if (a === 'key') {
+          out.loopKey = value
+        } else {
+          out.setAttribute(a, value)
+          if (a === 'p') {
+            const idx = value.indexOf('.')
+            if (idx === -1) {
+              comp[out.propKey = value] = out
+            } else {
+              const gcode = value.substring(0, idx)
+              if (!comp[gcode]) comp[gcode] = {}
+              comp[out.groupKey = gcode][out.propKey = value.substring(idx + 1)] = out
             }
+          }
         }
       }
     }
@@ -137,18 +145,18 @@ export function t (code) {
   return TRANS[code] || code
 }
 
-export function refreshTranslations(){
-    addDirty(translationUpdaters)
+export function refreshTranslations () {
+  addDirty(translationUpdaters)
 }
 
-function pushTranslationUpdater(u){
-    translationUpdaters.push(u)
+function pushTranslationUpdater (u) {
+  translationUpdaters.push(u)
 }
 
-export function T(code){
-    let out = function(){
-        return TRANS[code] || code
-    }
-    out.addUpdater = pushTranslationUpdater
-    return out
+export function T (code) {
+  const out = function () {
+    return TRANS[code] || code
+  }
+  out.addUpdater = pushTranslationUpdater
+  return out
 }
--- packages/web2/src/web.js
+++ packages/web2/src/web.js
@@ -1,40 +1,47 @@
-import { h, t,T, setTranslations, insertHtml, makeUpdater, refreshTranslations } from './jsxxx'
+import { h, t, T, setTranslations, insertHtml, makeUpdater, refreshTranslations } from './jsxxx'
 
-import test from './sample'
-
-async function changeLanguage(lang){
-    fetch(`locales/${lang}.json`).then(r=>r.text()).then((json)=>{
-        setTranslations(JSON.parse(json))
-        refreshTranslations()
-    })
+const langMap = {
+  en: 'english',
+  de: 'german',
+  fr: 'french',
+  ja: 'japanese',
+  hr: 'croatian'
 }
 
-changeLanguage('en').then(()=>{
-    let self = {}
-    const $ = makeUpdater()
-    
-    let count = 1
-    
-    let tpl =  <div p="main">
-        <label><input p="opts.autoReload" type="checkbox"/>{T`auto reload`}</label>
-        <label><input p="opts.autoRotate" type="checkbox"/>{T`auto rotate`}</label>
-        <label><input p="opts.autoZoom" type="checkbox"/>{T`auto zoom`}</label>
-        <label><input p="opts.showGrid" type="checkbox"/>{T`grid`}</label>
-        <label><input p="opts.showAxes" type="checkbox"/>{T`axes`}</label>
-        <div>
-            <button onclick={()=>$(count++)}>counter: {()=>count}</button>
-            {['en','hr','fr','de','ja'].map(l=>(
-            <button onclick={()=>changeLanguage(l)}>[{l}]</button>
-            ))}
-            
-        </div>
-    </div>    
-    
-    insertHtml(document.body,null,tpl,self, $)
-    
-    console.log('self', self, $)
-    $(count++)
+async function changeLanguage (lang) {
+  fetch(`locales/${lang}.json`).then(r => r.text()).then((json) => {
+    setTranslations(JSON.parse(json))
+    refreshTranslations()
+  })
+}
+
+changeLanguage('en').then(() => {
+  const self = {}
+  const $ = makeUpdater()
+
+  let count = 1
+
+  const tpl = (
+    <div p='main'>
+      <label><input p='opts.autoReload' type='checkbox' />{T`auto reload`}</label>
+      <label><input p='opts.autoRotate' type='checkbox' />{T`auto rotate`}</label>
+      <label><input p='opts.autoZoom' type='checkbox' />{T`auto zoom`}</label>
+      <label><input p='opts.showGrid' type='checkbox' />{T`grid`}</label>
+      <label><input p='opts.showAxes' type='checkbox' />{T`axes`}</label>
+      <div>
+        <button onclick={$(() => count++)}>test counter: {() => count}</button>
+        {Object.keys(langMap).map(l => (
+          <button p={`bt.${l}`} key={l} onclick={() => changeLanguage(l)}>{T(langMap[l])}</button>
+        ))}
+
+      </div>
+    </div>
+  )
+
+  insertHtml(document.body, null, tpl, self, $)
+
+  window.APP = self
+
+  console.log('self', self, $)
+  $(count++)
 })
-
-
-
