4655019bc825aac7bafc135b5e7b253928a8558b
Kenny Daniel <platypii@gmail.com>
2021-11-14 16:38:43 -0800

    Minimize faceNormal allocations

--- packages/modeling/src/geometries/poly3/faceNormal.js
+++ packages/modeling/src/geometries/poly3/faceNormal.js
@@ -1,34 +1,38 @@
 const vec3 = require('../../maths/vec3')
 
-/**
- * Calculate normal vector for a single vertex.
- */
-const vertexNormal = (vertices, index) => {
-  const a = vertices[index]
-  const b = vertices[(index + 1) % vertices.length]
-  const c = vertices[(index + 2) % vertices.length]
-  const ba = vec3.subtract(vec3.create(), b, a)
-  const ca = vec3.subtract(vec3.create(), c, a)
-  vec3.cross(ba, ba, ca)
-  vec3.normalize(ba, ba)
-  return ba
-}
 
 /**
  * Calculate normal vector for a face.
  * The normal vector is not always well defined.
  * A polygon may be non-planar. Vertices may be colinear.
- * To handle these cases we compute the average of the vertex normals.
+ * To handle these cases we compute the average vertex normal.
  */
 const faceNormal = (vertices) => {
-  if (vertices.length === 3) {
+  const len = vertices.length
+
+  // Calculate normal vector for a single vertex
+  // Inline to avoid allocations
+  const ba = vec3.create()
+  const ca = vec3.create()
+  const vertexNormal = (index) => {
+    const a = vertices[index]
+    const b = vertices[(index + 1) % len]
+    const c = vertices[(index + 2) % len]
+    vec3.subtract(ba, b, a) // ba = b - a
+    vec3.subtract(ca, c, a) // ca = c - a
+    vec3.cross(ba, ba, ca) // ba = ba x ca
+    vec3.normalize(ba, ba)
+    return ba
+  }
+
+  if (len === 3) {
     // optimization for triangles, which are always coplanar
-    return vertexNormal(vertices, 1)
+    return vertexNormal(0)
   } else {
-    // average of vertex normals
+    // sum of vertex normals
     const normal = vec3.create()
     vertices.forEach((v, i) => {
-      vec3.add(normal, normal, vertexNormal(vertices, i))
+      vec3.add(normal, normal, vertexNormal(i))
     })
     // renormalize normal vector
     vec3.normalize(normal, normal)
