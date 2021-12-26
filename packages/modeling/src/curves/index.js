87789dbcc5fbeac11a5c565777474052a2bb7cce
Z3 Development <z3-dev@gfnews.net>
2021-12-26 14:41:19 +0900

    docs(all): various document changes (#965)
    * docs(packages): new badge for cli dependencies
    
    * docs(packages): trying out downloads shields
    
    * doc(packages): another try at adding downloads
    
    * docs(packages): more download badges
    
    * docs(packages): more download badges for io
    
    * docs(packages): added some pointers on usage, and x3d-deserializer
    
    * docs(all): added curves to list
    
    * docs(all): lots of small fixes to documentation

--- packages/modeling/src/curves/index.js
+++ packages/modeling/src/curves/index.js
@@ -1,5 +1,5 @@
 /**
- * Curves are n-dimensional mathematical constructs that define a path from point 0 to point 1
+ * Curves are n-dimensional mathematical constructs that define a path from point 0 to point 1.
  * @module modeling/curves
  * @example
  * const { bezier } = require('@jscad/modeling').curves
