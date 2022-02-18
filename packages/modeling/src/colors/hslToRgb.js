8560b087f6d0005aa6b352c33d955a151953bdcb
Z3 Development <z3-dev@gfnews.net>
2022-02-18 16:55:11 +0900

    docs(modeling): doc changes from user reviews (#998)

--- packages/modeling/src/colors/hslToRgb.js
+++ packages/modeling/src/colors/hslToRgb.js
@@ -5,7 +5,7 @@ const hueToColorComponent = require('./hueToColorComponent')
 /**
  * Converts HSL color values to RGB color values.
  *
- * @see http://en.wikipedia.org/wiki/HSL_color_space.
+ * @see http://en.wikipedia.org/wiki/HSL_color_space
  * @param {...Number|Array} values - HSL or HSLA color values
  * @return {Array} RGB or RGBA color values
  * @alias module:modeling/colors.hslToRgb
