695d422555a9ee6597a6ede9b262e830d102715c
Chris Atkin <atkinchris@gmail.com>
2022-11-19 16:19:07 +0000

    Add specific overload for single geometry

--- packages/modeling/src/measurements/measureDimensions.d.ts
+++ packages/modeling/src/measurements/measureDimensions.d.ts
@@ -3,4 +3,5 @@ import RecursiveArray from '../utils/recursiveArray'
 
 export default measureDimensions
 
-declare function measureDimensions(...geometries: RecursiveArray<Geometry>): [number, number, number] | [number, number, number][]
+declare function measureDimensions(geometry: Geometry): [number, number, number]
+declare function measureDimensions(...geometries: RecursiveArray<Geometry>): [number, number, number][]
