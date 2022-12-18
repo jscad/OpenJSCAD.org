b9628b3399ddabea86a183faf7106de843ab8f1d
z3dev <z3-dev@gfnews.net>
2022-12-18 14:22:29 +0900

    build(dxf-deserializer): initial rollup config

---
+++ packages/io/dxf-deserializer/rollup.config.js
@@ -0,0 +1,26 @@
+import banner from 'rollup-plugin-banner'
+
+export default {
+  external: ['@jscad/modeling'],
+
+  input: 'src/index.js',
+
+  output: [
+    {
+      file: 'dist/jscad-dxf-deserializer.min.js',
+      format: 'umd',
+      name: 'jscadDxfSerializer',
+      globals: {
+        '@jscad/modeling': 'jscadModeling'
+      }
+    },
+    {
+      file: 'dist/jscad-dxf-deserializer.es.js',
+      format: 'es',
+    }
+  ],
+
+  plugins: [
+    banner('<%= pkg.description %>\n<%= pkg.name %>\nVersion <%= pkg.version %>\n<%= pkg.license %> License')
+  ]
+}
