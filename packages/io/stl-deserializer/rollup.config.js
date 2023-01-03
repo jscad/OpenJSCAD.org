f68fc3500b94833f69a9ba4dccbdbf8a3ddce19c
z3dev <z3-dev@gfnews.net>
2023-01-03 14:57:43 +0900

    build(stl-deserializer): initial rollup config

---
+++ packages/io/stl-deserializer/rollup.config.js
@@ -0,0 +1,31 @@
+import banner from 'rollup-plugin-banner'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import versionInjector from 'rollup-plugin-version-injector';
+
+
+export default {
+  external: ['@jscad/modeling'],
+
+  input: 'src/index.js',
+
+  output: [
+    {
+      file: 'dist/jscad-stl-deserializer.min.js',
+      format: 'umd',
+      name: 'jscadStlDeserializer',
+      globals: {
+        '@jscad/modeling': 'jscadModeling'
+      }
+    },
+    {
+      file: 'dist/jscad-stl-deserializer.es.js',
+      format: 'es',
+    }
+  ],
+
+  plugins: [
+    banner('<%= pkg.description %>\n<%= pkg.name %>\nVersion <%= pkg.version %>\n<%= pkg.license %> License'),
+    versionInjector({injectInComments:{fileRegexp: /\.(html)$/},logLevel: 'warn'}),
+    nodeResolve()
+  ]
+}
