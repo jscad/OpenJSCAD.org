20cf8287644ac497f74f4ff8f8045c0893f8623c
Kenny Daniel <platypii@gmail.com>
2023-01-08 17:14:25 -0800

    Add test for immutability of geom2.reverse

--- packages/modeling/src/geometries/geom2/reverse.test.js
+++ packages/modeling/src/geometries/geom2/reverse.test.js
@@ -1,10 +1,10 @@
 import test from 'ava'
 
-import { create, reverse } from './index.js'
+import { create, reverse, toPoints } from './index.js'
 
 import { comparePoints, compareVectors } from '../../../test/helpers/index.js'
 
-test('reverse: Reverses a populated geom2', (t) => {
+test('reverse: reverses a populated geom2', (t) => {
   const points = [[0, 0], [1, 0], [0, 1]]
   const expected = {
     outlines: [[[0, 1], [1, 0], [0, 0]]],
@@ -15,4 +15,17 @@ test('reverse: Reverses a populated geom2', (t) => {
   t.not(geometry, another)
   t.true(comparePoints(another.outlines[0], expected.outlines[0]))
   t.true(compareVectors(another.transforms, expected.transforms))
+})
+
+test('reverse: does not modify input geometry', (t) => {
+  const points = [[0, 0], [1, 0], [0, 1]]
+  // expected:
+  const forward = [[0, 0], [1, 0], [0, 1]]
+  const backward = [[0, 1], [1, 0], [0, 0]]
+
+  const geometry = create([points])
+  const another = reverse(geometry)
+  t.not(geometry, another)
+  t.true(comparePoints(toPoints(geometry), forward))
+  t.true(comparePoints(toPoints(another), backward))
 })
