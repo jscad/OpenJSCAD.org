5e6a792e374fed5c3c88f03b558c1231da2f206d
Bruce Borrett <oceativa@gmail.com>
2023-08-24 14:56:58 +0200

    Defined the entity type.

---
+++ packages/utils/regl-renderer/types/geometry-utils-V2/entity.d.ts
@@ -0,0 +1,3 @@
+import { Geometry } from '@jscad/modeling/src/geometries/types'
+
+export type Entity = { geometry: Geometry; visuals: { drawCmd: string; show: boolean; transparent: boolean; useVertexColors: boolean } };
