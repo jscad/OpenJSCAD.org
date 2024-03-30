abe9e96420fd76c3d09b33b903d7ed5e7a9a416a
iyegoroff <iegoroff@gmail.com>
2024-03-30 17:32:00 +0300

    fix types

--- packages/modeling/src/index.test-d.ts
+++ packages/modeling/src/index.test-d.ts
@@ -1,26 +1,34 @@
-import { expectType, expectNotType, expectError } from 'tsd'
+import { expectType, expectNotType, expectError, expectAssignable } from 'tsd'
 
 import { geometries } from './'
+import { snap, generalize } from './operations/modifiers'
 
 const { geom2 } = geometries
 
+expectAssignable<Function>(snap)
+expectAssignable<Function>(generalize)
 expectType(geom2.create())
 expectType<geom2.Geom2>(geom2.create([]))
 expectType<geom2.Geom2>(
-  geom2.create(
+  geom2.create([
     [
-      [[0, 0], [1, 1]],
-      [[1, 1], [2, 2]]
-    ]
-  )
+      [0, 0],
+      [1, 1],
+    ],
+    [
+      [1, 1],
+      [2, 2],
+    ],
+  ])
 )
 expectError(geom2.create([null]))
-expectNotType<
-  Parameters<typeof geom2.create>[0]
->(
+expectNotType<Parameters<typeof geom2.create>[0]>([
   [
-    [[0, 0, 0], [1, 1, 1]],
-    [[1, 1, 1], [2, 2, 2]]
-  ]
-)
-
+    [0, 0, 0],
+    [1, 1, 1],
+  ],
+  [
+    [1, 1, 1],
+    [2, 2, 2],
+  ],
+])
