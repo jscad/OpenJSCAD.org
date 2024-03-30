6710d0025de8b3b251305c72f021c44741d4b260
iyegoroff <iegoroff@gmail.com>
2024-03-30 17:35:55 +0300

    rollback format

--- packages/modeling/src/index.test-d.ts
+++ packages/modeling/src/index.test-d.ts
@@ -10,25 +10,20 @@ expectAssignable<Function>(generalize)
 expectType(geom2.create())
 expectType<geom2.Geom2>(geom2.create([]))
 expectType<geom2.Geom2>(
-  geom2.create([
+  geom2.create(
     [
-      [0, 0],
-      [1, 1],
-    ],
-    [
-      [1, 1],
-      [2, 2],
-    ],
-  ])
+      [[0, 0], [1, 1]],
+      [[1, 1], [2, 2]]
+    ]
+  )
 )
 expectError(geom2.create([null]))
-expectNotType<Parameters<typeof geom2.create>[0]>([
+expectNotType<
+  Parameters<typeof geom2.create>[0]
+>(
   [
-    [0, 0, 0],
-    [1, 1, 1],
-  ],
-  [
-    [1, 1, 1],
-    [2, 2, 2],
-  ],
-])
+    [[0, 0, 0], [1, 1, 1]],
+    [[1, 1, 1], [2, 2, 2]]
+  ]
+)
+
