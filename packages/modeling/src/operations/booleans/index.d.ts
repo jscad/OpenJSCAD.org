00915c6baeba2ffc770327e873b752a7271cac1c
张子涵 <2067519648@qq.com>
2024-04-30 09:52:38 +0800

    fix(modeling): create scission.d.ts and update index.d.ts

--- packages/modeling/src/operations/booleans/index.d.ts
+++ packages/modeling/src/operations/booleans/index.d.ts
@@ -1,5 +1,6 @@
 export { default as intersect } from './intersect'
 export { default as subtract } from './subtract'
 export { default as union } from './union'
+export { default as scission } from './scission'
 
 export as namespace booleans
