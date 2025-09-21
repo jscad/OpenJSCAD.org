119bd63197e800b429112f70e5d14fc6693425ae
z3dev <z3-dev@gfnews.net>
2025-09-21 13:33:50 +0900

    docs(array-utils): tweaks to docs and annotations

--- packages/array-utils/src/fnNumberSort.js
+++ packages/array-utils/src/fnNumberSort.js
@@ -1,9 +1,13 @@
 /**
  * Compare function for sorting arrays of numbers.
+ *
  * @param {number} a - first number
  * @param {number} b - second number
  * @return {number} result of a - b
+ *
  * @alias module:array-utils.fnNumberSort
+ * @function
+ *
  * @example
  * const numbers = [2, 1, 4, 3, 6, 5, 8, 7, 9, 0]
  * const sorted = numbers.sort(fnNumberSort)
--- packages/array-utils/src/toArray.js
+++ packages/array-utils/src/toArray.js
@@ -1,8 +1,12 @@
 /**
  * Convert the given array to an array if not already an array.
+ *
  * @param {*} array - anything
  * @returns {Array} an array
+ *
  * @alias module:array-utils.toArray
+ * @function
+ *
  * @example
  * const array = toArray(1) // [1]
  */
