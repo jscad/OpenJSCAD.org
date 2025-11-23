22321917fdfa9490dced284e914d7c0b02711129
z3dev <z3-dev@gfnews.net>
2025-11-24 08:31:37 +0900

    reorg(modeling): reworked some create functions to use JS defaults

--- packages/modeling/src/geometries/poly2/create.js
+++ packages/modeling/src/geometries/poly2/create.js
@@ -15,9 +15,4 @@
  * @example
  * let polygon = create([[0,0], [4,0], [4,3]])
  */
-export const create = (points) => {
-  if (points === undefined || points.length < 3) {
-    points = [] // empty contents
-  }
-  return { points }
-}
+export const create = (points = []) => ({ points })
