86c90ae63a54eaf50fe9e47dad6df161abc8f3f2
z3dev <z3-dev@gfnews.net>
2025-11-30 10:58:32 +0900

    feat(modeling): removed compact binary support

--- packages/modeling/src/geometries/geom3/toCompactBinary.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Geom3 } from './type.d.ts'
-
-export function toCompactBinary(geom: Geom3): Float32Array
--- packages/modeling/src/geometries/path2/fromCompactBinary.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Path2 } from './type.d.ts'
-
-export function fromCompactBinary(data: Array<number> | Float32Array | Float64Array): Path2
