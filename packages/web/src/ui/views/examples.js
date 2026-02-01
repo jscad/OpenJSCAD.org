1ab4dc3247a8e76ac2fc50423cc84009d9336c33
z3dev <z3-dev@gfnews.net>
2026-02-01 16:40:55 +0900

    fix(web): corrected data-path links in examples, and changed viewer to load an initial example

--- packages/web/src/ui/views/examples.js
+++ packages/web/src/ui/views/examples.js
@@ -6,17 +6,21 @@ const examplesData = require('../../../examples/examples.json')
 const examples = (state, i18n) => {
   const baseUrl = new URL(window.location.href)
   const origin = (baseUrl.origin === 'null') ? 'file://' : baseUrl.origin
-  let newPath = origin + path.dirname(baseUrl.pathname)
-  newPath = newPath.endsWith('/') ? newPath : newPath + path.sep
+  let newPath = origin + baseUrl.pathname
+  newPath = newPath.endsWith(path.sep) ? newPath : newPath + path.sep
 
   const examplesMenus = []
+  let groupId = 0
   for (const groupName in examplesData) {
+    groupId++
     if (!Object.prototype.hasOwnProperty.call(examplesData, groupName)) continue
-    const examplesElements = examplesData[groupName].map((example) =>
-      html`<li>
-          <a class="example" title="${example.description}" data-path="${newPath}${example.filePath}" href="#"> ${example.title} </a>
+
+    const examplesElements = examplesData[groupName].map((example, i) => {
+      const id = `example${groupId}-${i}`
+      return html`<li>
+          <a class="example" id="${id}" title="${example.description}" data-path="${newPath}${example.filePath}" href="#"> ${example.title} </a>
         </li>`
-    )
+    })
     examplesMenus.push(html`<li>${groupName}<ul class="examples-list">${examplesElements}</ul></li>`)
   }
   return html`<ul class="examples-group">${examplesMenus}</ul>`
