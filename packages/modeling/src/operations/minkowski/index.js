c3f4133bdbb39bd4fd7a6f2f1d44b88192cdc0a9
z3dev <z3-dev@gfnews.net>
2026-02-23 11:09:54 +0900

    feat(modeling): added minkowskiSum for geom3

---
+++ packages/modeling/src/operations/minkowski/index.js
@@ -0,0 +1,15 @@
+/**
+ * Minkowski sum operations for 3D geometries.
+ *
+ * The Minkowski sum of two shapes A and B is the set of all points that are
+ * the sum of a point in A and a point in B. This is useful for:
+ * - Offsetting/inflating shapes (using a sphere creates rounded edges)
+ * - Collision detection (shapes collide iff their Minkowski difference contains origin)
+ * - Motion planning and swept volumes
+ *
+ * @module modeling/minkowski
+ * @example
+ * import { minkowskiSum } from '@jscad/modeling'
+ * const rounded = minkowskiSum(cube, sphere)
+ */
+export { minkowskiSum } from './minkowskiSum.js'
