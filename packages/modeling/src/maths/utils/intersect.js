f0ce4ed3d775d6b1ceb9b3df2a579e9850e1e2e1
z3dev <z3-dev@gfnews.net>
2026-07-26 12:50:33 +0900

    fix(modeling): removed export of intersect from math utils due to conflicts

--- packages/modeling/src/maths/utils/intersect.js
+++ packages/modeling/src/maths/utils/intersect.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Calculate the intersect point of the two line segments (p1-p2 and p3-p4).
  * If the endpointTouch parameter is false, intersections at segment end points are excluded.
  * Note: If the line segments do NOT intersect then undefined is returned.
